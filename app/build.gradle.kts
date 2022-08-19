a2ffe211ff2db4b5eae280d173c1b772b9cb3bd8
Him188 <Him188@mamoe.net>
2022-08-20 00:32:16 +0800

    update copyright and license

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,21 @@
+/*
+ * Animation Garden App
+ * Copyright (C) 2022  Him188
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 @file:Suppress("OPT_IN_IS_NOT_ENABLED")
 
 import org.jetbrains.compose.ExperimentalComposeLibrary
@@ -39,12 +57,33 @@ dependencies {
 }
 
 compose.desktop {
-    application {
+
+application {
         mainClass = "me.him188.AnimationGardenDesktop"
         jvmArgs("--add-exports=java.desktop/com.apple.eawt=ALL-UNNAMED")
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = project.name
+            packageName = "Animation Garden Desktop"
+            description = "Desktop application for Animation Garden"
+            vendor = "Him188"
+            copyright = """
+                    Animation Garden App
+                    Copyright (C) 2022  Him188
+                    
+                    This program is free software: you can redistribute it and/or modify
+                    it under the terms of the GNU General Public License as published by
+                    the Free Software Foundation, either version 3 of the License, or
+                    (at your option) any later version.
+                    
+                    This program is distributed in the hope that it will be useful,
+                    but WITHOUT ANY WARRANTY; without even the implied warranty of
+                    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                    GNU General Public License for more details.
+                    
+                    You should have received a copy of the GNU General Public License
+                    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+            """.trimIndent()
+            licenseFile.set(rootProject.rootDir.resolve("LICENSE"))
             packageVersion = project.version.toString()
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,21 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+/*
+ * Animation Garden App
+ * Copyright (C) 2022  Him188
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 pluginManagement {
     repositories {
         google()
