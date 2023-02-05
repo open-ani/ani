b8835ff18733e1b06caa51ed639ec300af76fae2
Him188 <Him188@mamoe.net>
2023-02-05 12:27:06 +0000

    Add PreviewFilterChipRowByFlowRow

--- common/src/androidMain/kotlin/ui/search/FilterChipRow.kt
+++ common/src/androidMain/kotlin/ui/search/FilterChipRow.kt
@@ -18,19 +18,29 @@
 
 package me.him188.animationgarden.app.ui.search
 
-import androidx.compose.animation.*
-import androidx.compose.animation.core.*
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.SelectableChipElevation
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -80,20 +90,21 @@ actual fun <T> FilterChipRowImpl(
                 )
             )
         ) {
-            FlowRow(
-                mainAxisSpacing = 6.dp,
-                crossAxisSpacing = 8.dp,
-                mainAxisAlignment = FlowMainAxisAlignment.SpaceBetween,
-                lastLineMainAxisAlignment = FlowMainAxisAlignment.Start,
-            ) {
-                FilterChipRowByFlowRow(list, isSelected, onClick, content, enabled, elevation, refreshState)
-            }
+            FilterChipByFlowRow(
+                list,
+                isSelected,
+                onClick,
+                content,
+                enabled,
+                elevation,
+                refreshState
+            )
         }
     }
 }
 
 @Composable
-private fun <T> FilterChipRowByFlowRow(
+private fun <T> FilterChipByFlowRow(
     list: List<T>,
     isSelected: @Composable (T) -> Boolean,
     onClick: ((T) -> Unit)?,
@@ -102,36 +113,77 @@ private fun <T> FilterChipRowByFlowRow(
     elevation: SelectableChipElevation?,
     refreshState: RefreshState?,
 ) {
-    val showSuccessHint by animateFloatAsState(if (refreshState !is RefreshState.Success) 1f else 0f, tween(2000))
+    val showSuccessHint by animateFloatAsState(
+        if (refreshState !is RefreshState.Success) 1f else 0f,
+        tween(2000)
+    )
 
     val currentOnClick by rememberUpdatedState(onClick)
     val cardHeight = 32.dp
     val textHeight = 24.sp
     val progressSize = 18.dp
     val tickSize = 24.dp
-    for (it in list) {
-        ElevatedFilterChip(
-            selected = isSelected(it),
-            onClick = { currentOnClick?.invoke(it) },
-            label = {
-                ProvideTextStyle(LocalTextStyle.current.copy(lineHeight = textHeight)) {
-                    content(it)
-                }
-            },
-            enabled = enabled.invoke(it),
-            elevation = elevation,
-            modifier = Modifier.height(cardHeight),
+
+    FlowRow(
+        mainAxisSpacing = 6.dp,
+        crossAxisSpacing = 8.dp,
+        mainAxisAlignment = FlowMainAxisAlignment.SpaceBetween,
+        lastLineMainAxisAlignment = FlowMainAxisAlignment.Start,
+    ) {
+        for (it in list) {
+            ElevatedFilterChip(
+                selected = isSelected(it),
+                onClick = { currentOnClick?.invoke(it) },
+                label = {
+                    ProvideTextStyle(LocalTextStyle.current.copy(lineHeight = textHeight)) {
+                        content(it)
+                    }
+                },
+                enabled = enabled.invoke(it),
+                elevation = elevation,
+                modifier = Modifier.height(cardHeight),
 //                modifier = Modifier.animateItemPlacement(tween(200, 100)),
-        )
+            )
+        }
+        if (refreshState != null && (refreshState !is RefreshState.Success || showSuccessHint > 0)) {
+            RefreshingChip(
+                refreshState = refreshState,
+                textHeight = textHeight,
+                cardHeight = cardHeight,
+                progressSize = progressSize,
+                tickSize = tickSize,
+                elevation = elevation
+            )
+        }
     }
-    if (refreshState != null && (refreshState !is RefreshState.Success || showSuccessHint > 0)) {
-        RefreshingChip(
-            refreshState = refreshState,
-            textHeight = textHeight,
-            cardHeight = cardHeight,
-            progressSize = progressSize,
-            tickSize = tickSize,
-            elevation = elevation
-        )
-    }
+}
+
+@Preview(widthDp = 400, heightDp = 100)
+@Preview(widthDp = 300, heightDp = 100)
+@Preview(widthDp = 100, heightDp = 200)
+@Composable
+private fun PreviewFilterChipRowByFlowRow() {
+    data class Element(
+        val content: String,
+        val enabled: Boolean,
+        val selected: Boolean = false,
+    )
+
+    val elements = listOf(
+        Element("Foo", true),
+        Element("Foo", true),
+        Element("Foo", true),
+        Element("Bar", false),
+        Element("Zoo", true),
+    )
+
+    FilterChipByFlowRow(
+        list = elements,
+        isSelected = { it.selected },
+        onClick = {},
+        content = { Text(text = it.content) },
+        enabled = { true },
+        elevation = null,
+        refreshState = null,
+    )
 }
