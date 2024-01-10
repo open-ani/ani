0d9da545436fe823e213aef01c96465c227f42f5
Him188 <Him188@mamoe.net>
2024-01-10 14:12:18 +0000

    Remove duplicated copyright in buildSrc/sourceSets.kt

--- buildSrc/src/sourceSets.kt
+++ buildSrc/src/sourceSets.kt
@@ -29,25 +29,6 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.KotlinTarget
 
-/*
- * Ani
- * Copyright (C) 2022-2024 Him188
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-
 
 inline fun <reified T> Any?.safeAs(): T? {
     return this as? T
