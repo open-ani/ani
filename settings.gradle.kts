e6e6984300662a327bdd9bd37dc444c59871caee
Him188 <Him188@mamoe.net>
2024-01-12 01:24:54 +0000

    Add foojay resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,10 @@
 
 rootProject.name = "ani"
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
