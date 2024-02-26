69d96dd7fd43ea6946f308f257df820bab29c050
Him188 <Him188@mamoe.net>
2024-02-26 00:20:16 +0000

    Add black outlier to subtitles

--- app/video-player/androidMain/VideoPlayerView.android.kt
+++ app/video-player/androidMain/VideoPlayerView.android.kt
@@ -1,11 +1,14 @@
 package me.him188.ani.app.videoplayer
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.view.View
 import androidx.annotation.OptIn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.ui.CaptionStyleCompat
 import androidx.media3.ui.PlayerView
 import androidx.media3.ui.PlayerView.ControllerVisibilityListener
 
@@ -22,6 +25,18 @@ actual fun VideoPlayerView(
                 controllerAutoShow = false
                 useController = false
                 controllerHideOnTouch = false
+                subtitleView?.apply {
+                    this.setStyle(
+                        CaptionStyleCompat(
+                            Color.WHITE,
+                            0x000000FF,
+                            0x00000000,
+                            CaptionStyleCompat.EDGE_TYPE_OUTLINE,
+                            Color.BLACK,
+                            Typeface.DEFAULT,
+                        )
+                    )
+                }
                 (playerController as? ExoPlayerController)?.let {
                     player = it.player
                     setControllerVisibilityListener(ControllerVisibilityListener { visibility ->
