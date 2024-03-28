bfeb98b1a0d4d206f26a85d0d78b0502b787831e
Him188 <Him188@mamoe.net>
2024-03-28 03:00:40 +0000

    Fixup previous commit

--- app/shared/pages/profile/common/UserInfoRow.kt
+++ app/shared/pages/profile/common/UserInfoRow.kt
@@ -18,7 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -97,7 +97,7 @@ fun UserInfoRow(
                 Modifier,
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                var iconHeight by rememberSaveable { mutableStateOf(24.dp) }
+                var iconHeight by remember { mutableStateOf(24.dp) }
 //                Icon(
 //                    Icons.Default.SimCard, null,
 //                    Modifier
