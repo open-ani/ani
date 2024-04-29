2b7c6eaab197c14aa8d1acc014c62517640c2e72
JerryZ233 <zyq20033@163.com>
2024-04-29 22:06:32 +0100

    improve call logging

--- danmaku/ani/server/src/ktor/plugins/CallLogging.kt
+++ danmaku/ani/server/src/ktor/plugins/CallLogging.kt
@@ -1,12 +1,9 @@
 package me.him188.ani.danmaku.server.ktor.plugins
 
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.plugins.callloging.processingTimeMillis
-import io.ktor.server.plugins.origin
 import io.ktor.server.request.ApplicationRequest
 import io.ktor.server.request.httpMethod
 import io.ktor.server.request.path
@@ -19,15 +16,12 @@ internal fun Application.configureCallLogging() {
         }
         level = Level.INFO
         format { call ->
-            val remoteAddress = call.request.origin.remoteAddress
-            val remoteHost = call.request.origin.remoteHost
-            when (val status = call.response.status() ?: "Unhandled") {
-                HttpStatusCode.Found -> "$status: " +
-                        "${call.request.toLogString()} -> ${call.response.headers[HttpHeaders.Location]} " +
-                        "from $remoteAddress"
-
-                else -> "$status: ${call.request.toLogString()} from $remoteAddress ($remoteHost)"
-            }
+            """${call.response.status()}: ${call.request.toLogString()}. Request details:
+    Headers:
+        ${call.request.headers.entries().joinToString("\n        ") { (key, value) -> "$key=$value" }}
+    Queries:
+        ${call.request.queryParameters.entries().joinToString("\n        ") { (key, value) -> "$key=$value" }}
+            """
         }
     }
 }
