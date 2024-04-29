96fa56bbe6ebc049bc69992656308ae50f1dfbee
JerryZ233 <zyq20033@163.com>
2024-04-29 21:35:24 +0100

    improve call logging

--- danmaku/ani/server/src/ktor/plugins/CallLogging.kt
+++ danmaku/ani/server/src/ktor/plugins/CallLogging.kt
@@ -1,8 +1,15 @@
 package me.him188.ani.danmaku.server.ktor.plugins
 
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.plugins.callloging.processingTimeMillis
+import io.ktor.server.plugins.origin
+import io.ktor.server.request.ApplicationRequest
+import io.ktor.server.request.httpMethod
+import io.ktor.server.request.path
 import org.slf4j.event.Level
 
 internal fun Application.configureCallLogging() {
@@ -11,5 +18,20 @@ internal fun Application.configureCallLogging() {
             it.request.queryParameters["requestId"]
         }
         level = Level.INFO
+        format { call ->
+            val remoteAddress = call.request.origin.remoteAddress
+            val remoteHost = call.request.origin.remoteHost
+            when (val status = call.response.status() ?: "Unhandled") {
+                HttpStatusCode.Found -> "$status: " +
+                        "${call.request.toLogString()} -> ${call.response.headers[HttpHeaders.Location]} " +
+                        "from $remoteAddress"
+
+                else -> "$status: ${call.request.toLogString()} from $remoteAddress ($remoteHost)"
+            }
+        }
     }
+}
+
+private fun ApplicationRequest.toLogString(): String {
+    return "${httpMethod.value} - ${path()} in ${call.processingTimeMillis()}ms"
 }
