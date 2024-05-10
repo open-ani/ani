f9e74ead79a6776dbc390ee502e6f321005b0c04
Him188 <Him188@mamoe.net>
2024-05-10 01:41:44 +0100

    严格匹配剧集资源, close #250

--- data-sources/api/src/topic/TopicCriteria.kt
+++ data-sources/api/src/topic/TopicCriteria.kt
@@ -14,27 +14,25 @@ class TopicCriteria(
 
 fun TopicCriteria.matches(topic: Topic, allowEpMatch: Boolean): Boolean {
     val details = topic.details ?: return true
-    if (!isEpisodeSortMatch(details)) {
-        if (!allowEpMatch) return false
-        if (!isEpisodeEpMatch(details)) return false
-    }
-    return true
+    if (isEpisodeSortMatch(details)) return true
+    if (allowEpMatch && isEpisodeEpMatch(details)) return true
+    return false
 }
 
 private fun TopicCriteria.isEpisodeSortMatch(details: TopicDetails): Boolean {
     episodeSort?.let { expected ->
-        val ep = details.episodeRange
-        if (ep != null && expected !in ep) return false
+        val ep = details.episodeRange ?: return false
+        return expected in ep
     }
-    return true
+    return false
 }
 
 private fun TopicCriteria.isEpisodeEpMatch(details: TopicDetails): Boolean {
     episodeEp?.let { expected ->
-        val ep = details.episodeRange
-        if (ep != null && expected !in ep) return false
+        val ep = details.episodeRange ?: return false
+        return expected in ep
     }
-    return true
+    return false
 }
 
 
