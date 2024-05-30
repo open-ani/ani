e7adcdeb61e524a96b6fa5542066a9f3130e66a5
Him188 <Him188@mamoe.net>
2024-05-30 13:56:59 +0100

    Document MediaSource

--- data-sources/api/src/paging/Paged.kt
+++ data-sources/api/src/paging/Paged.kt
@@ -1,16 +1,34 @@
 package me.him188.ani.datasources.api.paging
 
 
+/**
+ * 分页数据的一页.
+ */
 data class Paged<T>(
+    /**
+     * 总共的数据数量 (不是本页数据数量), 可能为 `null` 表示未知.
+     */
     val total: Int?,
+    /**
+     * 是否还有更多页数
+     */
     val hasMore: Boolean,
+    /**
+     * 本页数据, 可能为空, 为空时 [hasMore] 一定为 `false`.
+     */
     val page: List<T>,
 ) {
     companion object {
+        /**
+         * 表示已经没有更多页数了
+         */
         fun <T> empty(): Paged<T> = Paged(null, false, emptyList())
     }
 }
 
+/**
+ * 根据一页数据 [list] 创建一个 [Paged] 对象, 当 [list] 为空时, 表示没有更多页数.
+ */
 fun <T> Paged(list: List<T>): Paged<T> {
     return Paged(null, hasMore = list.isNotEmpty(), list)
 }
