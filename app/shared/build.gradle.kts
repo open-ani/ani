open-ani/ani

commit a4272e2646cef11042af4c0c670c4e030be78350

Fix application id for update FileProvider

app/shared/build.gradle.kts
@@ -377,6 +377,10 @@ android {
             getDefaultProguardFile("proguard-android-optimize.txt"),
             *sharedAndroidProguardRules(),
         )
+        buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani.android\"")
+    }
+    buildTypes.getByName("debug") {
+        buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani.android.debug2\"")
     }
     buildFeatures {
         compose = true

app/android/src/main/AndroidManifest.xml
@@ -59,7 +59,7 @@
         </activity>
         <provider
                 android:name="androidx.core.content.FileProvider"
-                android:authorities="me.him188.ani.android.fileprovider"
+                android:authorities="${applicationId}.fileprovider"
                 android:exported="false"
                 android:grantUriPermissions="true">
             <meta-data

app/shared/pages/update/android/AndroidUpdateInstaller.kt
@@ -8,6 +8,7 @@ import android.net.Uri
 import android.provider.Settings
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.content.FileProvider
+import me.him188.ani.BuildConfig
 import me.him188.ani.app.platform.ContextMP
 import me.him188.ani.utils.logging.info
 import me.him188.ani.utils.logging.logger
@@ -59,7 +60,7 @@ class AndroidUpdateInstaller : UpdateInstaller {
 //        }
 //        file.copyTo(externalFile)
         intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive")
-        val apkUri = FileProvider.getUriForFile(context, "me.him188.ani.android.fileprovider", file)
+        val apkUri = FileProvider.getUriForFile(context, "${BuildConfig.APP_APPLICATION_ID}.fileprovider", file)
         intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)

commit 16dfe292774435557763f3ef3d304c12eec8da0d

修复安卓自动更新无法发起安装的问题
caused by a4272e2646cef11042af4c0c670c4e030be78350

app/shared/build.gradle.kts
@@ -377,10 +377,10 @@ android {
             getDefaultProguardFile("proguard-android-optimize.txt"),
             *sharedAndroidProguardRules(),
         )
-        buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani.android\"")
+        buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani\"")
     }
     buildTypes.getByName("debug") {
-        buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani.android.debug2\"")
+        buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani.debug2\"")
     }
     buildFeatures {
         compose = true