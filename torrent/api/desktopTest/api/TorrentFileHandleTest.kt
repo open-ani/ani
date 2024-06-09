f212748a3690ce81313d8c1b1fb7ab2657e29b20
Him188 <Him188@mamoe.net>
2024-06-09 19:19:06 +0100

    Fix TorrentFileHandleTest
    due to d324148e0a7b1e21e53ee3e5f2848add9d8ba27e

--- torrent/api/desktopTest/api/TorrentFileHandleTest.kt
+++ torrent/api/desktopTest/api/TorrentFileHandleTest.kt
@@ -3,7 +3,6 @@ package me.him188.ani.app.torrent.api
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import me.him188.ani.app.torrent.api.files.PieceState
-import me.him188.ani.app.torrent.api.handle.PieceFinishedEvent
 import me.him188.ani.app.torrent.api.handle.TorrentFinishedEvent
 import me.him188.ani.app.torrent.api.handle.TorrentThread
 import org.junit.jupiter.api.Test
@@ -41,7 +40,7 @@ internal class TorrentFileHandleTest : TorrentSessionSupport() {
             val file = getFiles().first()
             val fileHandle = file.createHandle()
             assertFalse(fileHandle.entry.stats.isFinished.first())
-            listener.onEvent(PieceFinishedEvent(handle.name, 0))
+            listener.onPieceFinished(0)
             assertFalse(fileHandle.entry.stats.isFinished.first())
             fileHandle.close()
         }
@@ -63,11 +62,11 @@ internal class TorrentFileHandleTest : TorrentSessionSupport() {
             fileHandle.entry.stats.run {
                 assertFalse(isFinished.first())
             }
-            listener.onEvent(PieceFinishedEvent(handle.name, 0))
+            listener.onPieceFinished(0)
             fileHandle.entry.stats.run {
                 assertFalse(isFinished.first())
             }
-            listener.onEvent(PieceFinishedEvent(handle.name, 1))
+            listener.onPieceFinished(1)
             fileHandle.entry.stats.run {
                 assertTrue(isFinished.first())
             }
@@ -91,11 +90,11 @@ internal class TorrentFileHandleTest : TorrentSessionSupport() {
             overallStats.run {
                 assertFalse(isFinished.first())
             }
-            listener.onEvent(PieceFinishedEvent(handle.name, 0))
+            listener.onPieceFinished(0)
             overallStats.run {
                 assertFalse(isFinished.first())
             }
-            listener.onEvent(PieceFinishedEvent(handle.name, 1))
+            listener.onPieceFinished(1)
             overallStats.run {
                 assertTrue(isFinished.first())
             }
@@ -122,14 +121,14 @@ internal class TorrentFileHandleTest : TorrentSessionSupport() {
                 assertEquals(0L, downloadedBytes.first())
                 assertEquals(1000, totalBytes.first())
             }
-            listener.onEvent(PieceFinishedEvent(handle.name, 0))
+            listener.onPieceFinished(0)
             fileHandle.entry.stats.run {
                 assertFalse(isFinished.first())
                 assertEquals(0.5f, progress.first())
                 assertEquals(500L, downloadedBytes.first())
                 assertEquals(1000, totalBytes.first())
             }
-            listener.onEvent(PieceFinishedEvent(handle.name, 1))
+            listener.onPieceFinished(1)
             fileHandle.entry.stats.run {
                 assertTrue(isFinished.first())
                 assertEquals(1f, progress.first())
