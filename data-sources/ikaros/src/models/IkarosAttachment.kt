82f3dacf7b96904f71aad8db9b4001a30ded2ea3
li-guohao <git@liguohao.cn>
2024-06-09 20:31:12 +0800

    optimize: IkarosAttachment.kt

--- data-sources/ikaros/src/models/IkarosAttachment.kt
+++ data-sources/ikaros/src/models/IkarosAttachment.kt
@@ -4,30 +4,30 @@ import kotlinx.serialization.Serializable
 import me.him188.ani.datasources.ikaros.models.IkarosAttachmentType
 
 @Serializable
-class IkarosAttachment {
-    val id: Long = 0
-    val parentId: Long = 0
-    val type: IkarosAttachmentType = IkarosAttachmentType.File
+class IkarosAttachment (
+    val id: Long = 0,
+    val parentId: Long = 0,
+    val type: IkarosAttachmentType = IkarosAttachmentType.File,
 
     /**
      * HTTP path.
      */
-    val url: String? = null
+    val url: String? = null,
 
     /**
      * Attachment logic path.
      */
-    val path: String? = null
+    val path: String? = null,
 
     /**
      * File path in file system.
      */
-    val fsPath: String? = null
+    val fsPath: String? = null,
 
     /**
      * filename with postfix.
      */
-    val name: String? = null
-    val size: Long = 0
-    val updateTime: String? = null
-}
+    val name: String? = null,
+    val size: Long = 0,
+    val updateTime: String? = null,
+)
