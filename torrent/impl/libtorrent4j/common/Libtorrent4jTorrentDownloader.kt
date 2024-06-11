06a16d7eca488d7067b934284283152b6d3bfcde
Him188 <Him188@mamoe.net>
2024-06-11 21:55:25 +0100

    Revert "允许同时 4 个 torrent 恢复检查 #255", fix #418
    This reverts commit df4618851a673f1b868d4722ed03f11f8bb5e80c.

--- torrent/impl/libtorrent4j/common/Libtorrent4jTorrentDownloader.kt
+++ torrent/impl/libtorrent4j/common/Libtorrent4jTorrentDownloader.kt
@@ -349,7 +349,6 @@ fun Libtorrent4jTorrentDownloader(
         uploadRateLimit(0)
         downloadRateLimit(0)
         maxPeerlistSize(1000)
-        activeChecking(4)
         dhtBootstrapNodes = setOf(
             dhtBootstrapNodes.split(",") + listOf(
                 "router.utorrent.com:6881",
