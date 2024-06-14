80ae1adaa816b501f1ddf4fa30c76f840e706f50
Him188 <Him188@mamoe.net>
2024-06-14 19:49:36 +0100

    [media] Extract TestMediaCacheEngine

--- data-sources/core/test/DirectoryMediaCacheStorageTest.kt
+++ data-sources/core/test/DirectoryMediaCacheStorageTest.kt
@@ -1,28 +1,19 @@
 package me.him188.ani.datasources.core.cache
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.test.runTest
-import me.him188.ani.datasources.api.CachedMedia
 import me.him188.ani.datasources.api.DefaultMedia
 import me.him188.ani.datasources.api.EpisodeSort
-import me.him188.ani.datasources.api.Media
 import me.him188.ani.datasources.api.MediaCacheMetadata
 import me.him188.ani.datasources.api.MediaProperties
 import me.him188.ani.datasources.api.source.MediaSourceKind
 import me.him188.ani.datasources.api.source.MediaSourceLocation
 import me.him188.ani.datasources.api.topic.EpisodeRange
-import me.him188.ani.datasources.api.topic.FileSize
-import me.him188.ani.datasources.api.topic.FileSize.Companion.bytes
 import me.him188.ani.datasources.api.topic.FileSize.Companion.megaBytes
 import me.him188.ani.datasources.api.topic.ResourceLocation
+import me.him188.ani.datasources.core.cache.framework.TestMediaCacheEngine
 import org.junit.jupiter.api.io.TempDir
 import java.io.File
-import java.util.concurrent.atomic.AtomicInteger
-import kotlin.coroutines.CoroutineContext
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertSame
@@ -35,72 +26,7 @@ class DirectoryMediaCacheStorageTest {
     @TempDir
     private lateinit var dir: File
 
-    private open class TestMediaCache(
-        val media: CachedMedia,
-        override val metadata: MediaCacheMetadata,
-        override val progress: Flow<Float> = MutableStateFlow(0f),
-        override val totalSize: Flow<FileSize> = MutableStateFlow(0.bytes),
-    ) : MediaCache {
-        override val origin: Media get() = media.origin
-        override suspend fun getCachedMedia(): CachedMedia = media
-        override fun isValid(): Boolean = true
-
-        override val downloadSpeed: Flow<FileSize> = MutableStateFlow(0.bytes)
-        override val uploadSpeed: Flow<FileSize> = MutableStateFlow(0.bytes)
-        override val finished: Flow<Boolean> by lazy {
-            progress.map { it == 1f }
-        }
-
-        val resumeCalled = AtomicInteger(0)
-
-        override suspend fun pause() {
-            println("pause")
-        }
-
-        override suspend fun resume() {
-            resumeCalled.incrementAndGet()
-            println("resume")
-        }
-
-        override val isDeleted: MutableStateFlow<Boolean> = MutableStateFlow(false)
-
-        override suspend fun delete() {
-            println("delete called")
-            isDeleted.value = true
-        }
-    }
-
-    private val engine = object : MediaCacheEngine {
-        override val isEnabled: Flow<Boolean> = flowOf(true)
-        override val stats: MediaStats = emptyMediaStats()
-        override fun supports(media: Media): Boolean = true
-
-        override suspend fun restore(
-            origin: Media,
-            metadata: MediaCacheMetadata,
-            parentContext: CoroutineContext
-        ): MediaCache? {
-            return null
-        }
-
-        override suspend fun createCache(
-            origin: Media,
-            metadata: MediaCacheMetadata,
-            parentContext: CoroutineContext
-        ): MediaCache {
-            return TestMediaCache(
-                media = CachedMedia(
-                    origin,
-                    CACHE_MEDIA_SOURCE_ID,
-                    download = origin.download,
-                ),
-                metadata = metadata.withExtra(mapOf("testExtra" to "1")),
-            )
-        }
-
-        override suspend fun deleteUnusedCaches(all: List<MediaCache>) {
-        }
-    }
+    private val engine = TestMediaCacheEngine(CACHE_MEDIA_SOURCE_ID)
 
     private val media = DefaultMedia(
         mediaId = "dmhy.2",
