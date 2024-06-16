ba012d35752fcb17f9f6fc4de8eabdd2533fbf0f
Him188 <Him188@mamoe.net>
2024-06-16 09:45:43 +0100

    Remove redundant onReplacement on mediaFetchSession

--- app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/EpisodeMediaFetchSession.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/EpisodeMediaFetchSession.kt
@@ -42,7 +42,6 @@ import me.him188.ani.datasources.core.fetch.MediaSourceResult
 import me.him188.ani.datasources.core.instance.MediaSourceInstance
 import me.him188.ani.utils.coroutines.OwnedCancellationException
 import me.him188.ani.utils.coroutines.checkOwner
-import me.him188.ani.utils.coroutines.onReplacement
 import me.him188.ani.utils.logging.info
 import me.him188.ani.utils.logging.logger
 import org.koin.core.context.GlobalContext
@@ -177,8 +176,6 @@ internal class DefaultEpisodeMediaFetchSession(
      */
     override val mediaFetchSession = combine(mediaFetchRequest, mediaFetcher) { req, fetcher ->
         fetcher.fetch(req)
-    }.onReplacement { 
-        
     }.shareInBackground(started = SharingStarted.Lazily)
 
     override val mediaFetcherCompleted by mediaFetchSession.flatMapLatest { it.hasCompleted }
