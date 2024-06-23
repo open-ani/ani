ca27c2c06883a14eb6fc23d1dfa95a8c4cdc1e60
Him188 <Him188@mamoe.net>
2024-06-23 21:05:28 +0100

    Extract EpisodePresentation and SubjectPresentation from EpisodeViewModel

--- app/shared/src/commonMain/kotlin/ui/subject/episode/EpisodeViewModel.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/EpisodeViewModel.kt
@@ -2,7 +2,6 @@ package me.him188.ani.app.ui.subject.episode
 
 import androidx.annotation.UiThread
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -42,14 +41,8 @@ import me.him188.ani.app.data.media.selector.eventHandling
 import me.him188.ani.app.data.models.VideoScaffoldConfig
 import me.him188.ani.app.data.repositories.EpisodePreferencesRepository
 import me.him188.ani.app.data.repositories.SettingsRepository
-import me.him188.ani.app.data.subject.EpisodeCollection
-import me.him188.ani.app.data.subject.SubjectInfo
 import me.him188.ani.app.data.subject.SubjectManager
-import me.him188.ani.app.data.subject.episode
 import me.him188.ani.app.data.subject.episodeInfoFlow
-import me.him188.ani.app.data.subject.isKnownBroadcast
-import me.him188.ani.app.data.subject.nameCnOrName
-import me.him188.ani.app.data.subject.renderEpisodeEp
 import me.him188.ani.app.data.subject.subjectInfoFlow
 import me.him188.ani.app.navigation.BrowserNavigator
 import me.him188.ani.app.platform.Context
@@ -78,84 +71,13 @@ import me.him188.ani.datasources.api.EpisodeSort
 import me.him188.ani.datasources.api.Media
 import me.him188.ani.datasources.api.source.MediaFetchRequest
 import me.him188.ani.datasources.api.topic.UnifiedCollectionType
-import me.him188.ani.datasources.bangumi.processing.nameCNOrName
 import me.him188.ani.utils.coroutines.cancellableCoroutineScope
 import me.him188.ani.utils.coroutines.runUntilSuccess
 import me.him188.ani.utils.coroutines.sampleWithInitial
 import me.him188.ani.utils.logging.info
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import org.openapitools.client.models.Episode
 import kotlin.time.Duration.Companion.milliseconds
-
-@Immutable
-class SubjectPresentation(
-    val title: String,
-    val isPlaceholder: Boolean = false,
-    val info: SubjectInfo,
-) {
-    companion object {
-        @Stable
-        val Placeholder = SubjectPresentation(
-            title = "placeholder",
-            isPlaceholder = true,
-            info = SubjectInfo.Empty,
-        )
-    }
-}
-
-/**
- * 展示在 UI 的状态
- */
-@Immutable
-class EpisodePresentation(
-    val episodeId: Int,
-    /**
-     * 剧集标题
-     * @see Episode.nameCNOrName
-     */
-    val title: String,
-    /**
-     * 在当前季度中的集数, 例如第二季的第一集为 01
-     *
-     * @see renderEpisodeEp
-     */
-    val ep: String,
-    /**
-     * 在系列中的集数, 例如第二季的第一集为 26
-     *
-     * @see renderEpisodeEp
-     */
-    val sort: String,
-    val collectionType: UnifiedCollectionType,
-    /**
-     * 是否已经确定开播了
-     */
-    val isKnownBroadcast: Boolean,
-    val isPlaceholder: Boolean = false,
-) {
-    companion object {
-        @Stable
-        val Placeholder = EpisodePresentation(
-            episodeId = -1,
-            title = "placeholder",
-            ep = "placeholder",
-            sort = "placeholder",
-            collectionType = UnifiedCollectionType.WISH,
-            isKnownBroadcast = false,
-            isPlaceholder = true,
-        )
-    }
-}
-
-fun EpisodeCollection.toPresentation() = EpisodePresentation(
-    episodeId = this.episode.id,
-    title = episode.nameCnOrName,
-    ep = episode.renderEpisodeEp(),
-    sort = episode.sort.toString(),
-    collectionType = collectionType,
-    isKnownBroadcast = episode.isKnownBroadcast,
-)
 
 
 @Stable
---
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/SubjectPresentation.kt
@@ -0,0 +1,21 @@
+package me.him188.ani.app.ui.subject.episode
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+import me.him188.ani.app.data.subject.SubjectInfo
+
+@Immutable
+class SubjectPresentation(
+    val title: String,
+    val isPlaceholder: Boolean = false,
+    val info: SubjectInfo,
+) {
+    companion object {
+        @Stable
+        val Placeholder = SubjectPresentation(
+            title = "placeholder",
+            isPlaceholder = true,
+            info = SubjectInfo.Empty,
+        )
+    }
+}
