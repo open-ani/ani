18f318639998951d9fe8cd90e6729d44b96c4c5a
Him188 <Him188@mamoe.net>
2024-06-30 20:18:53 +0100

    Remove obsolete code

--- app/shared/src/commonMain/kotlin/ui/subject/details/SubjectDetailsPage.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/details/SubjectDetailsPage.kt
@@ -6,42 +6,23 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.OpenInNew
-import androidx.compose.material.icons.outlined.ChatBubbleOutline
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SecondaryScrollableTabRow
 import androidx.compose.material3.Tab
@@ -51,7 +32,6 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
@@ -64,25 +44,21 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import kotlinx.coroutines.launch
 import me.him188.ani.app.platform.LocalContext
 import me.him188.ani.app.platform.Platform
 import me.him188.ani.app.platform.isMobile
-import me.him188.ani.app.ui.external.placeholder.placeholder
 import me.him188.ani.app.ui.foundation.layout.ConnectedScrollState
 import me.him188.ani.app.ui.foundation.layout.connectedScrollContainer
 import me.him188.ani.app.ui.foundation.layout.connectedScrollTarget
 import me.him188.ani.app.ui.foundation.layout.rememberConnectedScrollState
 import me.him188.ani.app.ui.foundation.pagerTabIndicatorOffset
-import me.him188.ani.app.ui.foundation.theme.weaken
 import me.him188.ani.app.ui.foundation.widgets.FastLinearProgressIndicator
 import me.him188.ani.app.ui.foundation.widgets.FastLinearProgressState
 import me.him188.ani.app.ui.foundation.widgets.TopAppBarGoBackButton
@@ -97,8 +73,6 @@ import me.him188.ani.app.ui.subject.details.components.SubjectDetailsDefaults
 import me.him188.ani.app.ui.subject.details.components.SubjectDetailsHeader
 import me.him188.ani.app.ui.subject.rating.EditRatingDialog
 import me.him188.ani.app.ui.subject.rating.EditRatingState
-import me.him188.ani.datasources.bangumi.models.BangumiEpisode
-import me.him188.ani.datasources.bangumi.processing.fixToString
 
 @Composable
 fun SubjectDetailsScene(
@@ -379,153 +353,5 @@ private fun renderSubjectDetailsTab(tab: SubjectDetailsTab): String {
         SubjectDetailsTab.DETAILS -> "详情"
         SubjectDetailsTab.COMMENTS -> "短评"
         SubjectDetailsTab.DISCUSSIONS -> "讨论"
-    }
-}
-
-
-@Composable
-private fun <T> PersonList(
-    list: List<T>?,
-    key: (T) -> Any,
-    horizontalPadding: Dp,
-    modifier: Modifier = Modifier,
-    each: @Composable (T) -> Unit,
-) {
-    val spacedBy = 16.dp
-    LazyRow(
-        modifier = modifier.placeholder(visible = list == null).fillMaxWidth().heightIn(min = 100.dp),
-        horizontalArrangement = Arrangement.spacedBy(spacedBy),
-    ) {
-        item(key = "spacer header") { Spacer(Modifier.width(horizontalPadding - spacedBy)) }
-        items(list.orEmpty(), key = key) { item ->
-            each(item)
-        }
-        item(key = "spacer footer") { Spacer(Modifier.width(horizontalPadding - spacedBy)) }
-    }
-}
-
-@Composable
-private fun PersonView(
-    avatar: @Composable () -> Unit,
-    text: @Composable () -> Unit,
-    role: @Composable () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column(modifier.width(64.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onBackground) {
-            Box(Modifier.clip(CircleShape).size(64.dp)) {
-                avatar()
-            }
-            Box(Modifier.padding(top = 4.dp)) {
-                ProvideTextStyle(MaterialTheme.typography.bodySmall) {
-                    text()
-                }
-            }
-            Box(Modifier.padding(top = 4.dp)) {
-                ProvideTextStyle(MaterialTheme.typography.labelSmall) {
-                    CompositionLocalProvider(LocalContentColor provides LocalContentColor.current.weaken()) {
-                        role()
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-private fun EpisodeList(
-    episodes: List<BangumiEpisode>,
-    horizontalPadding: Dp,
-    onClickItem: (BangumiEpisode) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val horizontalSpacedBy = 8.dp
-    val onClickItemState by rememberUpdatedState(onClickItem)
-    LazyHorizontalGrid(
-        GridCells.Fixed(1),
-        modifier = modifier.height(60.dp).fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(horizontalSpacedBy),
-    ) {
-        item(key = "spacer header") { Spacer(Modifier.width(horizontalPadding - horizontalSpacedBy)) }
-        items(episodes, key = { it.id }) { episode ->
-            EpisodeItem(episode, { onClickItemState(episode) }, Modifier.widthIn(min = 60.dp, max = 160.dp))
-        }
-        item(key = "spacer footer") { Spacer(Modifier.width(horizontalPadding - horizontalSpacedBy)) }
-    }
-}
-
-/**
- * 一个剧集:
- * ```
- * |------------|
- * | 01 冒险结束 |
- * |       评论 |
- * |------------|
- * ```
- */
-@Composable
-fun EpisodeItem(
-    episode: BangumiEpisode,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val shape = RoundedCornerShape(8.dp)
-    Card(
-        onClick,
-        modifier.clip(shape),
-        shape = shape,
-//        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp),
-    ) {
-        Column(
-            Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-            Arrangement.spacedBy(8.dp),
-        ) {
-            Row(
-                Modifier,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                // "01"
-                Text(episode.sort.fixToString(2, '0'), style = MaterialTheme.typography.bodyMedium)
-
-                Spacer(Modifier.weight(1f, fill = false))
-
-                // "冒险结束"
-                Text(
-                    episode.nameCn,
-                    Modifier.padding(start = 8.dp),
-                    style = MaterialTheme.typography.bodyMedium,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-            }
-
-            Row(
-                Modifier.align(Alignment.End),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Icon(
-                    Icons.Outlined.ChatBubbleOutline,
-                    null,
-                    Modifier.size(16.dp),
-                )
-                Text(
-                    remember { "${episode.comment}" },
-                    Modifier.offset(y = (-1).dp).padding(start = 4.dp),
-                    style = MaterialTheme.typography.bodySmall,
-                )
-            }
-        }
-    }
-}
-
-@Composable
-private fun SectionTitle(modifier: Modifier = Modifier, text: @Composable () -> Unit) {
-    Row(modifier.padding(top = 8.dp, bottom = 8.dp)) {
-        ProvideTextStyle(MaterialTheme.typography.titleMedium) {
-            CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onBackground) {
-                text()
-            }
-        }
     }
 }
