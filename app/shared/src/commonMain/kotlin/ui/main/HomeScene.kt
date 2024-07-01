a692e1dd9ab69edafae33a56251a972627d456b8
Him188 <Him188@mamoe.net>
2024-07-01 22:29:45 +0100

    当应用重启回到主页时取消设置全屏, close #490

--- app/shared/src/commonMain/kotlin/ui/main/HomeScene.kt
+++ app/shared/src/commonMain/kotlin/ui/main/HomeScene.kt
@@ -50,6 +50,9 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import me.him188.ani.app.navigation.LocalNavigator
 import me.him188.ani.app.platform.LocalContext
+import me.him188.ani.app.platform.currentPlatform
+import me.him188.ani.app.platform.isAndroid
+import me.him188.ani.app.platform.setRequestFullScreen
 import me.him188.ani.app.session.isLoggedIn
 import me.him188.ani.app.session.requireOnline
 import me.him188.ani.app.tools.update.InstallationFailureReason
@@ -75,6 +78,13 @@ import moe.tlaster.precompose.flow.collectAsStateWithLifecycle
 
 @Composable
 fun HomeScene(modifier: Modifier = Modifier) {
+    if (currentPlatform.isAndroid()) {
+        val context = LocalContext.current
+        SideEffect {
+            context.setRequestFullScreen(false)
+        }
+    }
+
     if (isShowLandscapeUI()) {
         HomeSceneLandscape(modifier)
     } else {
