8db8a38d311a251d04ff84768476f39652acd26f
Him188 <Him188@mamoe.net>
2024-07-03 22:08:17 +0100

    add windows installer

---
+++ ci-helper/install-deps-windows.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# 这个文件会在 GitHub Actions 的 Windows runner 上执行
+
+# C++
+choco install cmake
+choco install ninja
+choco install llvm
+
+# libtorrent
+choco install swig
+choco install boost-msvc-14.1
