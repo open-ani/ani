4c4a9462243267d1f358c4126147b9f07cf036de
Him188 <Him188@mamoe.net>
2024-07-03 22:21:02 +0100

    fix include

--- torrent/anitorrent/include/anitorrent.h
+++ torrent/anitorrent/include/anitorrent.h
@@ -4,14 +4,13 @@
 #include "libtorrent/session.hpp"
 
 namespace anilt {
-    extern "C" {
-    std::string lt_version();
+extern "C" {
+std::string lt_version();
 
-    struct stats_t {
-    };
+struct stats_t {};
 
 // allocate from Java
-    struct torrent_info_t {
+struct torrent_info_t {
         std::string name;
         std::string infohash_hex;
         int file_count = 0;
@@ -20,27 +19,26 @@ namespace anilt {
         std::string error_message;
 
         std::shared_ptr<libtorrent::torrent_info> info;
-    };
+};
 
-    bool torrent_info_parse(torrent_info_t &ti, const std::string &encoded);
+bool torrent_info_parse(torrent_info_t &ti, const std::string &encoded);
 
-    void torrent_info_release(torrent_info_t &ti);
+void torrent_info_release(torrent_info_t &ti);
 
 
 // allocate from Java
-    struct torrent_handle_t {
+struct torrent_handle_t {
         std::shared_ptr<libtorrent::torrent_handle> delegate;
-    };
+};
 
-    libtorrent::session *new_session(const char *user_agent);
+libtorrent::session *new_session(const char *user_agent);
 
 // stats get_stats(libtorrent::session &s) {}
-    std::string fetch_magnet(libtorrent::session *s, const std::string &uri, int timeout_seconds,
-                             const std::string &save_path);
+std::string fetch_magnet(libtorrent::session *s, const std::string &uri, int timeout_seconds,
+                         const std::string &save_path);
 
-    bool
-    start_download(libtorrent::session *s, torrent_handle_t &handle, const torrent_info_t &info,
-                   const std::string &save_path);
+bool start_download(libtorrent::session *s, torrent_handle_t &handle, const torrent_info_t &info,
+                    const std::string &save_path);
 
 } // namespace anilt
 } // namespace anilt
