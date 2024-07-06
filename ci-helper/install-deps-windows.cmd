e098eb4a76faeb5a5c24a9b8a1dbfe26d420ffe0
Him188 <Him188@mamoe.net>
2024-07-06 20:57:19 +0100

    update

--- ci-helper/install-deps-windows.cmd
+++ ci-helper/install-deps-windows.cmd
@@ -1,6 +1,9 @@
 @REM 这个文件会在 GitHub Actions 的 Windows runner 上执行
 
 vcpkg install boost-system:x64-windows
+vcpkg install boost-range:x64-windows
+vcpkg install boost-parameter:x64-windows
+vcpkg install boost-asio:x64-windows
 
 choco install swig -y
 
