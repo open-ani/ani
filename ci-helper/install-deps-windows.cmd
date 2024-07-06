4b67c66e6470bbfd08d47146ad55384cc41b07e8
Him188 <Him188@mamoe.net>
2024-07-06 16:22:24 +0100

    fix

--- ci-helper/install-deps-windows.cmd
+++ ci-helper/install-deps-windows.cmd
@@ -1,10 +1,9 @@
 # 这个文件会在 GitHub Actions 的 Windows runner 上执行
 
 # C++
-choco install cmake
-choco install ninja
-
+vcpkg install openssl:x64-windows
+vcpkg install boost
+vcpkg install cmake
+vcpkg install ninja
 # libtorrent
 choco install swig
-choco install boost-msvc-14.1
-choco install openssl
