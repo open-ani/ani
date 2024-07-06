fb04f296f4490bb67093a085a6f7c11706cd2995
Him188 <Him188@mamoe.net>
2024-07-06 18:56:58 +0100

    remove brew -y

--- ci-helper/install-deps-macos.sh
+++ ci-helper/install-deps-macos.sh
@@ -3,11 +3,11 @@
 # 这个文件会在 GitHub Actions 的 macOS runner 上执行
 
 # C++
-brew install cmake -y
-brew install ninja -y
-brew install llvm -y
+brew install cmake
+brew install ninja
+brew install llvm
 
 # libtorrent
-brew install swig -y
-brew install openssl -y
-brew install boost -y
+brew install swig
+brew install openssl
+brew install boost
