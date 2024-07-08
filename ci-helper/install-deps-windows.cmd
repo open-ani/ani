b0ecd38c945c25e3187fb7727db9ac0e8eeda7ab
Him188 <Him188@mamoe.net>
2024-07-08 20:46:42 +0100

    install openssl

--- ci-helper/install-deps-windows.cmd
+++ ci-helper/install-deps-windows.cmd
@@ -2,6 +2,7 @@
 
 @REM 直接用 vcpkg install boost 也可以跑, 但是这会在 CI 上装一小时, 所以就只装了最少的能过编译的包
 
+vcpkg install openssl:x64-windows
 vcpkg install boost-variant:x64-windows
 vcpkg install boost-system:x64-windows
 vcpkg install boost-range:x64-windows
