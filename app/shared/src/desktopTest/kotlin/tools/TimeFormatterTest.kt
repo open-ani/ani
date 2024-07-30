f8b37683b6a95e351c039c6a89a9a545c8c184e3
Hacbit <107751081+hacbit@users.noreply.github.com>
2024-07-31 01:40:22 +0800

    修复 TimeFormatterTest.kt 中的 testUsingFormatter() 用例
    修复因时区问题导致的解析结果不匹配

--- app/shared/src/desktopTest/kotlin/tools/TimeFormatterTest.kt
+++ app/shared/src/desktopTest/kotlin/tools/TimeFormatterTest.kt
@@ -1,6 +1,9 @@
 package me.him188.ani.app.tools
 
 import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datatime.toLocalDataTime
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.format.char
 import kotlin.test.Test
@@ -78,6 +81,8 @@ class TimeFormatterTest {
     @Test
     fun testUsingFormatter() {
         val timestamp = Instant.parse("2019-12-30T10:00:00Z")
+            .toLocalDataTime(TimeZone.UTC)
+            .toInstant(TimeZone.currentSystemDefault())
         assertEquals("2019-12-30 10:00:00", timeFormatter.format(timestamp))
     }
 }
