483728b67c78f20201c27c8d9718d991bbbf692a
Him188 <Him188@mamoe.net>
2024-08-07 03:51:48 +0800

    仅在 debug 模式下显示重力感应旋屏设置, #686

--- app/shared/src/commonMain/kotlin/ui/settings/tabs/app/AppSettingsTab.kt
+++ app/shared/src/commonMain/kotlin/ui/settings/tabs/app/AppSettingsTab.kt
@@ -36,6 +36,7 @@ import me.him188.ani.app.platform.isDesktop
 import me.him188.ani.app.platform.isMobile
 import me.him188.ani.app.tools.update.supportsInAppUpdate
 import me.him188.ani.app.ui.external.placeholder.placeholder
+import me.him188.ani.app.ui.foundation.isInDebugMode
 import me.him188.ani.app.ui.foundation.rememberViewModel
 import me.him188.ani.app.ui.settings.SettingsTab
 import me.him188.ani.app.ui.settings.framework.AbstractSettingsViewModel
@@ -420,7 +421,7 @@ private fun SettingsScope.PlayerGroup(
             title = { Text("选择数据源后自动关闭弹窗") },
             Modifier.placeholder(vm.uiSettings.loading),
         )
-        if (currentPlatform.isMobile()) {
+        if (currentPlatform.isMobile() && isInDebugMode()) {
             HorizontalDividerItem()
             SwitchItem(
                 checked = config.autoFullscreenOnLandscapeMode,
