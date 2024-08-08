8db6f7838f6fea3edea34ad3c1ee0ecf4d10ca25
Him188 <Him188@mamoe.net>
2024-08-08 23:49:03 +0800

    Revert "windows音量控制 (仅限 debug 模式) (#676)"
    This reverts commit 4b00997e770666d8f09c30d695c2ac602c34ad09.

--- app/shared/video-player/src/androidMain/kotlin/PlayerState.android.kt
+++ app/shared/video-player/src/androidMain/kotlin/PlayerState.android.kt
@@ -355,7 +355,6 @@ internal class ExoPlayerState @UiThread constructor(
     override val subtitleTracks: MutableTrackGroup<SubtitleTrack> = MutableTrackGroup()
 
     override val audioTracks: MutableTrackGroup<AudioTrack> = MutableTrackGroup()
-
     override fun saveScreenshotFile(filename: String) {
         TODO("Not yet implemented")
     }
