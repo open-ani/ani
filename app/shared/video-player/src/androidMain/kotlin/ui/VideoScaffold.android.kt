53b9422a8f5b6a488c57a98cdc330fc5576ace94
Him188 <Him188@mamoe.net>
2024-08-09 13:52:08 +0800

    Remove dependency on PlayerState from MediaProgressSliderState

--- app/shared/video-player/src/androidMain/kotlin/ui/VideoScaffold.android.kt
+++ app/shared/video-player/src/androidMain/kotlin/ui/VideoScaffold.android.kt
@@ -127,7 +127,7 @@ private fun PreviewVideoScaffoldImpl(
         detachedProgressSlider = {
             PlayerControllerDefaults.MediaProgressSlider(
                 progressSliderState,
-                playerState,
+                cacheProgressState = playerState.cacheProgress,
                 enabled = false,
             )
         },
