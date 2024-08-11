a79fd1097435da42b8800212a545390759985de8
chivehao <git@liguohao.cn>
2024-08-12 06:09:18 +0800

    optimize: datasource EpisodeType

--- data-sources/api/src/commonMain/kotlin/EpisodeType.kt
+++ data-sources/api/src/commonMain/kotlin/EpisodeType.kt
@@ -1,26 +1,14 @@
 package me.him188.ani.datasources.api
 
 import kotlinx.serialization.Serializable
-import kotlin.jvm.JvmInline
 
-/**
- * [Bangumi API](https://bangumi.github.io/api) EpisodeType
- */
-@JvmInline
 @Serializable
-value class EpisodeType(val value: Int) {
-    companion object {
-        val MainStory = EpisodeType(0)
-        val SP = EpisodeType(1)
-
-        val OP = EpisodeType(2)
-
-        val ED = EpisodeType(3)
-        
-        val PV = EpisodeType(4)
-        
-        val MAD = EpisodeType(5)
-        
-        val OTHER = EpisodeType(6)
-    }
+enum class EpisodeType(val value: Int) {
+    MainStory(0),
+    SP(1),
+    OP(2),
+    ED(3),
+    PV(4),
+    MAD(5),
+    OTHER(6)
 }
