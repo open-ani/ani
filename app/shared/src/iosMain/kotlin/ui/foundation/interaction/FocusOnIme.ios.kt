1ce05ce6328aa48c3705ab356f8d149f8d27ed0d
Him188 <Him188@mamoe.net>
2024-08-12 01:40:11 +0800

    Suppress inlining warnings on `isImeVisible` on iOS

--- app/shared/src/iosMain/kotlin/ui/foundation/interaction/FocusOnIme.ios.kt
+++ app/shared/src/iosMain/kotlin/ui/foundation/interaction/FocusOnIme.ios.kt
@@ -3,6 +3,7 @@ package me.him188.ani.app.ui.foundation.interaction
 import androidx.compose.runtime.Composable
 
 @Composable
+@Suppress("NOTHING_TO_INLINE", "KotlinRedundantDiagnosticSuppress")
 actual inline fun isImeVisible(): Boolean {
     // TODO IOS isImeVisible
     return false
