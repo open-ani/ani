c2fec83327a0a94780c57c285098057bcaff2909
StageGuard <sg@mamoe.net>
2024-08-13 22:07:22 +0800

    remember `listBottomPadding` in `EpisodeCommentColumn`

--- app/shared/src/commonMain/kotlin/ui/subject/episode/comments/EpisodeCommentColumn.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/comments/EpisodeCommentColumn.kt
@@ -22,6 +22,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
@@ -62,7 +63,9 @@ fun EpisodeCommentColumn(
 
     val navigationBars = WindowInsets.navigationBars
     var stubPanelHeight by rememberSaveable { mutableStateOf(0) }
-    val listBottomPadding by derivedStateOf { navigationBars.getBottom(density) + stubPanelHeight }
+    val listBottomPadding by remember {
+        derivedStateOf { navigationBars.getBottom(density) + stubPanelHeight }
+    }
 
     LaunchedEffect(true) {
         launch {
