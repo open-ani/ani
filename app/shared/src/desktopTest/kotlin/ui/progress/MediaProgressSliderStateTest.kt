18b922816d34f933dece09d7e99e213c432f5a13
Nier4ever <20170127nwl@gmail.com>
2024-08-13 08:58:22 +0800

    optimize something and add test

--- app/shared/src/desktopTest/kotlin/ui/progress/MediaProgressSliderStateTest.kt
+++ app/shared/src/desktopTest/kotlin/ui/progress/MediaProgressSliderStateTest.kt
@@ -1,4 +1,4 @@
-package ui.progress
+package me.him188.ani.app.ui.progress
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
@@ -9,13 +9,13 @@ import me.him188.ani.app.ui.framework.runComposeStateTest
 import me.him188.ani.app.ui.framework.takeSnapshot
 import me.him188.ani.app.videoplayer.ui.progress.MediaProgressSliderState
 import me.him188.ani.app.videoplayer.ui.state.Chapter
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class MediaProgressSliderStateTest {
 
     @Test
-    fun testMediaProgressSliderState() = runComposeStateTest {
+    fun `preview and finish preview then play`() = runComposeStateTest {
         val chapters = persistentListOf<Chapter>()
         var positionState by mutableLongStateOf(0L)
 
@@ -31,6 +31,34 @@ class MediaProgressSliderStateTest {
 
         state.previewPositionRatio(0.5f)
         state.finishPreview()
+        takeSnapshot()
+        assertEquals(0.5f, state.displayPositionRatio)
+        positionState += 5_000L
+        takeSnapshot()
+        assertEquals(0.55f, state.displayPositionRatio)
+    }
+
+    @Test
+    fun `preview and play 5s and finish preview then play`() = runComposeStateTest {
+        val chapters = persistentListOf<Chapter>()
+        var positionState by mutableLongStateOf(0L)
+
+        val state = MediaProgressSliderState(
+            currentPositionMillis = { positionState },
+            totalDurationMillis = { 100_000L },
+            chapters = mutableStateOf(chapters),
+            onPreview = {},
+            onPreviewFinished = {
+                positionState = it
+            },
+        )
+
+        state.previewPositionRatio(0.5f)
+        positionState += 5_000L
+        takeSnapshot()
+        assertEquals(0.5f, state.displayPositionRatio)
+        state.finishPreview()
+        takeSnapshot()
         assertEquals(0.5f, state.displayPositionRatio)
         positionState += 5_000L
         takeSnapshot()
