b3a8b0fb5062ac1ed89517c2f4737ccfba02c367
StageGuard <sg@mamoe.net>
2024-08-13 20:16:58 +0800

    move converter from data object to repository

--- app/shared/src/commonMain/kotlin/data/models/subject/SubjectComment.kt
+++ app/shared/src/commonMain/kotlin/data/models/subject/SubjectComment.kt
@@ -2,7 +2,6 @@ package me.him188.ani.app.data.models.subject
 
 import androidx.compose.runtime.Immutable
 import me.him188.ani.app.data.models.UserInfo
-import me.him188.ani.datasources.bangumi.next.models.BangumiNextSubjectInterestCommentListInner
 
 @Immutable
 data class SubjectComment(
@@ -14,19 +13,4 @@ data class SubjectComment(
     val content: String,
     val creator: UserInfo?,
     val rating: Int
-)
-
-fun BangumiNextSubjectInterestCommentListInner.toSubjectComment() = SubjectComment(
-    id = 31 * comment.hashCode() + 31 * updatedAt + 31 * (user?.id ?: UserInfo.EMPTY.id),
-    content = comment,
-    updatedAt = updatedAt,
-    rating = rate,
-    creator = user?.let { u ->
-        UserInfo(
-            id = u.id,
-            nickname = u.nickname,
-            username = u.id.toString(),
-            avatarUrl = u.avatar.medium,
-        ) // 没有username
-    },
 )
--- app/shared/src/commonMain/kotlin/data/repository/CommentRepository.kt
+++ app/shared/src/commonMain/kotlin/data/repository/CommentRepository.kt
@@ -1,9 +1,9 @@
 package me.him188.ani.app.data.repository
 
+import me.him188.ani.app.data.models.UserInfo
 import me.him188.ani.app.data.models.episode.EpisodeComment
 import me.him188.ani.app.data.models.episode.toEpisodeComment
 import me.him188.ani.app.data.models.subject.SubjectComment
-import me.him188.ani.app.data.models.subject.toSubjectComment
 import me.him188.ani.datasources.api.paging.PageBasedPagedSource
 import me.him188.ani.datasources.api.paging.Paged
 import me.him188.ani.datasources.api.paging.PagedSource
@@ -28,10 +28,12 @@ class BangumiCommentRepositoryImpl(
         return PageBasedPagedSource { page ->
             try {
                 // 第一页先获取所有评论数量，获取失败则继续使用时间正序查询，也不会提供总计大小
-                if (page == 0) try {
-                    val response = client.getNextApi().subjectComments(subjectId, 1, 1).body()
-                    setTotalSize(response.total)
-                } catch (_: Exception) {
+                if (page == 0) {
+                    try {
+                        val response = client.getNextApi().subjectComments(subjectId, 1, 1).body()
+                        setTotalSize(response.total)
+                    } catch (_: Exception) {
+                    }
                 }
 
                 val total = totalSize
@@ -74,3 +76,18 @@ class BangumiCommentRepositoryImpl(
         }
     }
 }
+
+private fun BangumiNextSubjectInterestCommentListInner.toSubjectComment() = SubjectComment(
+    id = 31 * comment.hashCode() + 31 * updatedAt + 31 * (user?.id ?: UserInfo.EMPTY.id),
+    content = comment,
+    updatedAt = updatedAt,
+    rating = rate,
+    creator = user?.let { u ->
+        UserInfo(
+            id = u.id,
+            nickname = u.nickname,
+            username = u.id.toString(),
+            avatarUrl = u.avatar.medium,
+        ) // 没有username
+    },
+)
