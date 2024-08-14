9f6dab19916259a5db48874258cf7fe99195518b
chivehao <git@liguohao.cn>
2024-08-14 16:20:56 +0800

    optimize: remove code in EpisodeType

--- data-sources/api/src/commonMain/kotlin/EpisodeType.kt
+++ data-sources/api/src/commonMain/kotlin/EpisodeType.kt
@@ -3,13 +3,13 @@ package me.him188.ani.datasources.api
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class EpisodeType(val code: Int, val value: String) {
-    MainStory(0, "MainStory"),
-    SP(1, "SP"),
-    OP(2, "OP"),
-    ED(3, "ED"),
-    PV(4, "PV"),
-    MAD(5, "MAD"),
-    OTHER(6, "OTHER");
+enum class EpisodeType(val value: String) {
+    MainStory("MainStory"),
+    SP("SP"),
+    OP("OP"),
+    ED("ED"),
+    PV("PV"),
+    MAD("MAD"),
+    OTHER("OTHER");
 }
 
