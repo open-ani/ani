c122f95dadb63bab6454ac328c13a581df5aa313
Him188 <Him188@mamoe.net>
2024-08-15 02:06:56 +0800

    Extract CommentDefaults from Comment.kt

--- app/shared/src/commonMain/kotlin/ui/subject/components/comment/Comment.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/components/comment/Comment.kt
@@ -1,38 +1,22 @@
 package me.him188.ani.app.ui.subject.components.comment
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AddReaction
-import androidx.compose.material.icons.outlined.HeartBroken
-import androidx.compose.material.icons.outlined.ModeComment
-import androidx.compose.material.icons.outlined.MoreHoriz
-import androidx.compose.material.icons.outlined.Report
-import androidx.compose.material.icons.rounded.Face
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
@@ -41,29 +25,17 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import me.him188.ani.app.data.models.UserInfo
-import me.him188.ani.app.data.source.BangumiCommentSticker
 import me.him188.ani.app.tools.MonoTasker
-import me.him188.ani.app.ui.foundation.LocalIsPreviewing
-import me.him188.ani.app.ui.foundation.avatar.AvatarImage
 import me.him188.ani.app.ui.foundation.isInDebugMode
-import me.him188.ani.app.ui.foundation.richtext.RichText
-import me.him188.ani.app.ui.foundation.richtext.RichTextDefaults
 import me.him188.ani.app.ui.foundation.richtext.UIRichElement
 import me.him188.ani.app.ui.foundation.theme.slightlyWeaken
-import me.him188.ani.app.ui.foundation.theme.stronglyWeaken
-import me.him188.ani.app.ui.subject.components.comment.EditCommentDefaults.ActionButton
-import me.him188.ani.app.ui.subject.components.comment.EditCommentDefaults.ActionButtonSize
-import org.jetbrains.compose.resources.painterResource
 
 /**
  * 评论项目
@@ -236,214 +208,3 @@ class UICommentReaction(
     val selected: Boolean
 )
 
-object CommentDefaults {
-    @Composable
-    fun Avatar(url: String?) {
-        AvatarImage(
-            url = url,
-            modifier = Modifier.size(36.dp),
-        )
-    }
-
-    @Composable
-    fun Reaction(
-        reaction: UICommentReaction,
-        onClick: () -> Unit,
-        modifier: Modifier = Modifier
-    ) {
-        val backgroundColor by animateColorAsState(
-            if (reaction.selected) {
-                MaterialTheme.colorScheme.secondaryContainer
-            } else {
-                Color.Transparent
-            },
-        )
-        Surface(
-            onClick = onClick,
-            modifier = Modifier
-                .minimumInteractiveComponentSize()
-                .then(modifier),
-            enabled = true,
-            shape = CircleShape,
-            color = backgroundColor,
-            border = SuggestionChipDefaults.suggestionChipBorder(true),
-        ) {
-            Row(
-                modifier = Modifier.padding(horizontal = 6.dp, vertical = 2.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                val previewing = LocalIsPreviewing.current
-                val reactionDrawableRes = BangumiCommentSticker[reaction.id]
-
-                if (previewing || reactionDrawableRes == null) Icon(
-                    imageVector = Icons.Rounded.Face,
-                    modifier = Modifier.padding(end = 4.dp).size(24.dp),
-                    contentDescription = null,
-                ) else Icon(
-                    painter = painterResource(reactionDrawableRes),
-                    modifier = Modifier.padding(end = 4.dp).size(24.dp),
-                    contentDescription = null,
-                )
-
-                Text(
-                    text = reaction.count.toString(),
-                    style = MaterialTheme.typography.labelMedium,
-                    modifier = Modifier.padding(end = 4.dp),
-                )
-            }
-        }
-    }
-
-    @Composable
-    fun ReactionRow(
-        list: List<UICommentReaction>,
-        onClickItem: (reactionId: Int) -> Unit,
-        modifier: Modifier = Modifier,
-    ) {
-        FlowRow(
-            modifier = modifier,
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
-        ) {
-            list.forEach {
-                Reaction(
-                    reaction = it,
-                    modifier = Modifier.padding(vertical = 6.dp),
-                    onClick = { onClickItem(it.id) },
-                )
-            }
-        }
-    }
-
-    @Composable
-    fun ActionRow(
-        onClickReply: () -> Unit,
-        modifier: Modifier = Modifier,
-        onClickReaction: () -> Unit,
-        onClickBlock: () -> Unit,
-        onClickReport: () -> Unit
-    ) {
-        val size = ActionButtonSize.dp
-        val iconSize = 20.dp
-        var actionRowExpanded by rememberSaveable { mutableStateOf(false) }
-        val expandableActionWidth by animateDpAsState(if (actionRowExpanded) size else 0.dp)
-        
-        Row(modifier = modifier) { 
-            CompositionLocalProvider(
-                LocalContentColor provides MaterialTheme.colorScheme.onSurface.stronglyWeaken(),
-            ) {
-                ActionButton(
-                    imageVector = Icons.Outlined.ModeComment,
-                    contentDescription = "回复评论",
-                    onClick = onClickReply,
-                    iconSize = iconSize,
-                )
-
-                if (actionRowExpanded) {
-                    ActionButton(
-                        imageVector = Icons.Outlined.AddReaction,
-                        contentDescription = "添加表情",
-                        onClick = onClickReaction,
-                        iconSize = iconSize,
-                        modifier = Modifier.size(height = size, width = expandableActionWidth),
-                    )
-                    ActionButton(
-                        imageVector = Icons.Outlined.HeartBroken,
-                        contentDescription = "拉黑用户",
-                        onClick = onClickBlock,
-                        iconSize = iconSize,
-                        modifier = Modifier.size(height = size, width = expandableActionWidth),
-                    )
-                    ActionButton(
-                        imageVector = Icons.Outlined.Report,
-                        contentDescription = "举报用户",
-                        onClick = onClickReport,
-                        iconSize = iconSize,
-                        modifier = Modifier.size(height = size, width = expandableActionWidth),
-                    )
-                }
-
-                // 最后一个按钮不要有 ripple effect，因为有动画，看起来比较奇怪
-                ActionButton(
-                    imageVector = Icons.Outlined.MoreHoriz,
-                    contentDescription = "展开更多评论功能",
-                    iconSize = iconSize,
-                    onClick = { actionRowExpanded = true },
-                    modifier = Modifier.size(height = size, width = size - expandableActionWidth),
-                    hasIndication = false,
-                )
-            }
-        }
-    }
-
-    @Composable
-    fun ReplyList(
-        replies: List<UIComment>,
-        onClickUrl: (String) -> Unit,
-        onClickExpand: () -> Unit,
-        modifier: Modifier = Modifier,
-        hiddenReplyCount: Int = 0
-    ) {
-        val primaryColor = MaterialTheme.colorScheme.primary
-
-        Column(modifier = modifier) {
-            replies.forEach { reply ->
-                val prepended = remember(reply.content, primaryColor) {
-                    reply.content.prependText(
-                        prependix = "${reply.creator?.nickname ?: reply.creator?.id.toString()}：",
-                        color = primaryColor,
-                    )
-                }
-                RichText(
-                    elements = prepended.elements,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 8.dp, vertical = 4.dp),
-                    onClickUrl = onClickUrl,
-                )
-            }
-            if (hiddenReplyCount > 0) {
-                Text(
-                    text = "查看更多 $hiddenReplyCount 条回复>",
-                    color = primaryColor,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 8.dp, vertical = 4.dp)
-                        .clickable(onClick = onClickExpand),
-                )
-            }
-        }
-    }
-
-    // prepend text
-    private fun UIRichText.prependText(prependix: String, color: Color): UIRichText = run {
-        // 如果 elements 是空的则直接返回一个 annotated text
-        val first = elements.firstOrNull()
-            ?: return@run listOf(
-                UIRichElement.AnnotatedText(
-                    listOf(UIRichElement.Annotated.Text(prependix, RichTextDefaults.FontSize, color)),
-                ),
-            )
-
-        // 如果第一个 element 是 annotated text，则把 prepend 添加到其中
-        if (first is UIRichElement.AnnotatedText) {
-            listOf(
-                first.copy(
-                    slice = listOf(
-                        UIRichElement.Annotated.Text(prependix, RichTextDefaults.FontSize, color),
-                        *first.slice.toTypedArray(),
-                    ),
-                ),
-                *elements.drop(1).toTypedArray(),
-            )
-        } else { // 如果不是就添加一个 annotated text
-            listOf(
-                UIRichElement.AnnotatedText(
-                    listOf(UIRichElement.Annotated.Text(prependix, RichTextDefaults.FontSize, color)),
-                ),
-                *elements.toTypedArray(),
-            )
-        }
-    }.let {
-        UIRichText(it)
-    }
-}
