7f4824ef2d6445444cb05f7a5387c2740a0d4177
StageGuard <sg@mamoe.net>
2024-08-19 00:27:30 +0800

    revert `remove dummy item of SubjectCommentColumn, fix #758`

--- app/shared/src/commonMain/kotlin/ui/subject/details/components/SubjectComment.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/details/components/SubjectComment.kt
@@ -3,8 +3,10 @@ package me.him188.ani.app.ui.subject.details.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -77,6 +79,7 @@ fun SubjectDetailsDefaults.SubjectCommentColumn(
             state = listState,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            item("spacer header") { Spacer(Modifier.height(1.dp)) }
             itemsIndexed(state.list, key = { _, item -> item.id }) { index, item ->
                 SubjectComment(
                     comment = item,
