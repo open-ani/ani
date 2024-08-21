0dd356f03010db08985a5ec9bcdea6ebe5772de2
Him188 <Him188@mamoe.net>
2024-08-21 20:12:11 +0100

    Revert "Add ios app launcher"
    This reverts commit 5a09f722dbd11543a88000043b408afdc5c90e0b.

--- app/shared/build.gradle.kts
+++ app/shared/build.gradle.kts
@@ -54,16 +54,6 @@ atomicfu {
 }
 
 kotlin {
-    listOf(
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-    
     sourceSets.commonMain.dependencies {
         api(libs.kotlinx.coroutines.core)
         api(libs.kotlinx.serialization.json)
