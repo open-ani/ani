open-ani/ani

commit 9f5dc62b59665ee243e878c9b276e8cb8336d0db

Fix subject cache states, fix #804 缓存番剧界面快速点击下载会弹出存储位置对话框

app/shared/src/androidMain/kotlin/ui/subject/cache/CacheListGroup.android.kt
@@ -133,7 +133,7 @@ private fun PreviewEpisodeItem() = ProvideCompositionLocalsForPreview {
                         EpisodeCacheState(
                             0,
                             { EpisodeCacheRequester(flowOf(), MediaSelectorFactory.withKoin(), flowOf()) },
-                            info = MutableStateFlow(
+                            infoState = MutableStateFlow(
                                 EpisodeCacheInfo(
                                     sort = EpisodeSort(++id),
                                     ep = null,
@@ -142,7 +142,7 @@ private fun PreviewEpisodeItem() = ProvideCompositionLocalsForPreview {
                                     hasPublished = true,
                                 ),
                             ),
-                            cacheStatusFlow = MutableStateFlow(EpisodeCacheStatus.NotCached),
+                            cacheStatusState = MutableStateFlow(EpisodeCacheStatus.NotCached),
                             EmptyCoroutineContext,
                         )
                     },
@@ -164,7 +164,7 @@ private fun PreviewEpisodeItemVeryLong() = ProvideCompositionLocalsForPreview {
                 EpisodeCacheState(
                     0,
                     { EpisodeCacheRequester(flowOf(), MediaSelectorFactory.withKoin(), flowOf()) },
-                    info = MutableStateFlow(
+                    infoState = MutableStateFlow(
                         EpisodeCacheInfo(
                             sort = EpisodeSort(1),
                             ep = null,
@@ -173,7 +173,7 @@ private fun PreviewEpisodeItemVeryLong() = ProvideCompositionLocalsForPreview {
                             hasPublished = true,
                         ),
                     ),
-                    cacheStatusFlow = MutableStateFlow(EpisodeCacheStatus.NotCached),
+                    cacheStatusState = MutableStateFlow(EpisodeCacheStatus.NotCached),
                     EmptyCoroutineContext,
                 )
             },

app/shared/src/commonMain/kotlin/ui/subject/cache/EpisodeCacheListState.kt
@@ -11,10 +11,7 @@ import me.him188.ani.app.data.source.media.cache.requester.EpisodeCacheRequester
 import me.him188.ani.app.data.source.media.cache.requester.trySelectSingle
 import me.him188.ani.app.data.source.media.cache.storage.MediaCacheStorage
 import me.him188.ani.app.data.source.media.selector.MediaSelector
-import me.him188.ani.app.ui.foundation.BackgroundScope
-import me.him188.ani.app.ui.foundation.HasBackgroundScope
 import me.him188.ani.datasources.api.Media
-import kotlin.coroutines.CoroutineContext
 
 @Stable
 interface EpisodeCacheListState {
@@ -88,22 +85,30 @@ interface EpisodeCacheListState {
 @Stable
 class EpisodeCacheListStateImpl(
     episodes: State<List<EpisodeCacheState>>,
+    currentEpisode: State<EpisodeCacheState?>,
+    // background scope
     private val onRequestCache: suspend (episode: EpisodeCacheState, autoSelectByCached: Boolean) -> CacheRequestStage?,
     private val onRequestCacheComplete: suspend (episode: EpisodeCacheTargetInfo) -> Unit,
     private val onDeleteCache: suspend (episode: EpisodeCacheState) -> Unit,
-    parentCoroutineContext: CoroutineContext,
-) : HasBackgroundScope by BackgroundScope(parentCoroutineContext), EpisodeCacheListState {
+) : EpisodeCacheListState {
     override val episodes: List<EpisodeCacheState> by episodes
 
     override val anyEpisodeActionRunning: Boolean by derivedStateOf {
         this.episodes.any { it.actionTasker.isRunning }
     }
 
-    override val currentEpisode: EpisodeCacheState? by derivedStateOf {
-        this.episodes.firstOrNull { it.currentSelectMediaTask != null || it.currentSelectStorageTask != null }
-    }
+    override val currentEpisode: EpisodeCacheState? by currentEpisode
     override val currentSelectMediaTask: SelectMediaTask? by derivedStateOf {
-        this.episodes.firstNotNullOfOrNull { it.currentSelectMediaTask }
+        val current = this.currentEpisode
+        val stage = current?.currentStage
+        // 只要是 working 就要有这个, 否则选 storage 的时候 media 的 sheet 就被关闭了
+        if (stage !is CacheRequestStage.Working) return@derivedStateOf null
+        SelectMediaTask(
+            episode = current,
+            fetchSession = stage.fetchSession,
+            mediaSelector = stage.mediaSelector,
+            attemptedTrySelect = stage.attemptedTrySelect,
+        )
     }
 
     override fun selectMedia(media: Media) {
@@ -125,7 +130,14 @@ class EpisodeCacheListStateImpl(
     }
 
     override val currentSelectStorageTask: SelectStorageTask? by derivedStateOf {
-        this.episodes.firstNotNullOfOrNull { it.currentSelectStorageTask }
+        val current = this.currentEpisode
+        val stage = current?.currentStage
+        if (stage !is CacheRequestStage.SelectStorage) return@derivedStateOf null
+        SelectStorageTask(
+            episode = current,
+            options = stage.storages,
+            attemptedTrySelect = stage.attemptedTrySelect,
+        )
     }
 
     override fun selectStorage(storage: MediaCacheStorage) {
@@ -161,7 +173,6 @@ class EpisodeCacheListStateImpl(
         episode.actionTasker.launch {
             // TODO: 处理错误 
             onRequestCache(episode, autoSelectCached)?.let {
-                episode.currentSelectStorageTask
                 if (it is CacheRequestStage.Done) {
                     callComplete(episode, it)
                 }

app/shared/src/commonMain/kotlin/ui/subject/cache/EpisodeCacheState.kt
@@ -3,10 +3,10 @@
 package me.him188.ani.app.ui.subject.cache
 
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.StateFlow
 import me.him188.ani.app.data.source.media.cache.EpisodeCacheStatus
 import me.him188.ani.app.data.source.media.cache.engine.MediaCacheEngine
@@ -18,33 +18,29 @@ import me.him188.ani.app.data.source.media.cache.storage.MediaCacheStorage
 import me.him188.ani.app.data.source.media.fetch.MediaFetchSession
 import me.him188.ani.app.data.source.media.selector.MediaSelector
 import me.him188.ani.app.tools.MonoTasker
-import me.him188.ani.app.ui.foundation.BackgroundScope
-import me.him188.ani.app.ui.foundation.HasBackgroundScope
 import me.him188.ani.datasources.api.CachedMedia
 import me.him188.ani.datasources.api.Media
 import me.him188.ani.datasources.api.MediaCacheMetadata
 import me.him188.ani.datasources.api.topic.isDoneOrDropped
-import kotlin.coroutines.CoroutineContext
 
 
 /**
  * 一个条目的一个剧集的缓存状态
  */
 @Stable
-open class EpisodeCacheState(
+class EpisodeCacheState(
     val episodeId: Int,
-    cacheRequesterLazy: () -> EpisodeCacheRequester,
-    info: Flow<EpisodeCacheInfo>,
-    cacheStatusFlow: Flow<EpisodeCacheStatus>,
-    parentCoroutineContext: CoroutineContext,
-) : HasBackgroundScope by BackgroundScope(parentCoroutineContext) {
-    internal val cacheRequester by lazy(cacheRequesterLazy)
-
+    val cacheRequester: EpisodeCacheRequester,
+    currentStageState: State<CacheRequestStage>,
+    infoState: State<EpisodeCacheInfo>,
+    cacheStatusState: State<EpisodeCacheStatus?>,
+    backgroundScope: CoroutineScope,
+) {
     /**
      * [episodeId] 对应的剧集信息, 初始值为 [EpisodeCacheInfo.Placeholder], 表示正在加载.
      * @see isInfoLoading
      */
-    val info: EpisodeCacheInfo by info.produceState(EpisodeCacheInfo.Placeholder)
+    val info: EpisodeCacheInfo by infoState
 
     /**
      * [info] 是否仍然在加载中
@@ -53,40 +49,12 @@ open class EpisodeCacheState(
         this.info === EpisodeCacheInfo.Placeholder
     }
 
-    private val currentStage: CacheRequestStage by cacheRequester.stage.produceState()
-
-    /**
-     * 当前需要展示给用户选择的 [MediaSelector]
-     */
-    val currentSelectMediaTask by derivedStateOf {
-        (currentStage as? CacheRequestStage.Working)?.let {
-            SelectMediaTask(
-                episode = this,
-                fetchSession = it.fetchSession,
-                mediaSelector = it.mediaSelector,
-                attemptedTrySelect = it.attemptedTrySelect,
-            )
-        }
-    }
-
-    /**
-     * 当前需要展示给用户选择的存储位置列表
-     */
-    val currentSelectStorageTask by derivedStateOf {
-        (currentStage as? CacheRequestStage.SelectStorage)?.let {
-            SelectStorageTask(
-                episode = this,
-                options = it.storages,
-                attemptedTrySelect = it.attemptedTrySelect,
-            )
-        }
-    }
-
+    val currentStage by currentStageState
 
     /**
      * 该剧集当前的缓存状态. `null` 表示还在加载中
      */
-    val cacheStatus: EpisodeCacheStatus? by cacheStatusFlow.produceState(null)
+    val cacheStatus: EpisodeCacheStatus? by cacheStatusState
 
     /**
      * 是否允许缓存该剧集
@@ -102,7 +70,7 @@ open class EpisodeCacheState(
     val actionTasker = MonoTasker(backgroundScope)
 
     val showProgressIndicator by derivedStateOf {
-        actionTasker.isRunning || currentStage is CacheRequestStage.Working
+        actionTasker.isRunning || currentStageState.value is CacheRequestStage.Working
     }
 }
 
@@ -111,7 +79,7 @@ sealed class AbstractTask {
 }
 
 /**
- * @see EpisodeCacheState.currentSelectMediaTask
+ * @see EpisodeCacheState.currentStage
  */
 @Stable
 class SelectMediaTask(
@@ -147,22 +115,3 @@ data class EpisodeCacheTargetInfo(
     val metadata: MediaCacheMetadata,
 )
 
-
-///////////////////////////////////////////////////////////////////////////
-// Testing
-///////////////////////////////////////////////////////////////////////////
-
-@Stable
-class TestEpisodeCacheState(
-    episodeId: Int,
-    cacheRequesterLazy: () -> EpisodeCacheRequester,
-    val infoFlow: MutableStateFlow<EpisodeCacheInfo>,
-    val cacheStatusFlow: MutableStateFlow<EpisodeCacheStatus>,
-    parentCoroutineContext: CoroutineContext,
-) : EpisodeCacheState(
-    episodeId,
-    cacheRequesterLazy,
-    infoFlow,
-    cacheStatusFlow,
-    parentCoroutineContext,
-)

app/shared/src/commonMain/kotlin/ui/subject/cache/SubjectCacheScene.kt
@@ -20,15 +20,17 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.retry
 import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 import me.him188.ani.app.data.models.episode.displayName
 import me.him188.ani.app.data.models.episode.isKnownCompleted
 import me.him188.ani.app.data.models.preference.MediaSelectorSettings
@@ -49,6 +51,8 @@ import me.him188.ani.app.navigation.LocalNavigator
 import me.him188.ani.app.ui.external.placeholder.placeholder
 import me.him188.ani.app.ui.foundation.AbstractViewModel
 import me.him188.ani.app.ui.foundation.launchInBackground
+import me.him188.ani.app.ui.foundation.produceState
+import me.him188.ani.app.ui.foundation.stateOf
 import me.him188.ani.app.ui.foundation.widgets.TopAppBarGoBackButton
 import me.him188.ani.app.ui.settings.SettingsTab
 import me.him188.ani.app.ui.settings.framework.components.SettingsScope
@@ -69,13 +73,6 @@ interface SubjectCacheViewModel {
     val cacheListState: EpisodeCacheListState
 }
 
-open class TestSubjectCacheViewModel(
-    override val subjectId: Int,
-    override val subjectTitle: String?,
-    override val cacheListState: EpisodeCacheListState,
-    override val mediaSelectorSettingsFlow: Flow<MediaSelectorSettings>
-) : SubjectCacheViewModel
-
 @Stable
 class SubjectCacheViewModelImpl(
     override val subjectId: Int,
@@ -93,33 +90,37 @@ class SubjectCacheViewModelImpl(
     private val episodeCollectionsFlow = subjectManager.episodeCollectionsFlow(subjectId).retry()
         .shareInBackground()
 
-    private val episodesFlow = episodeCollectionsFlow.take(1).mapLatest { episodes ->
-        // 每个 episode 都为一个 flow, 然后合并
-        episodes.map { episodeCollection ->
-            val episode = episodeCollection.episodeInfo
+    private val episodesFlow = episodeCollectionsFlow.take(1).transformLatest { episodes ->
+        supervisorScope {
+            // 每个 episode 都为一个 flow, 然后合并
+            emit(
+                episodes.map { episodeCollection ->
+                    val episode = episodeCollection.episodeInfo
 
-            val cacheStatusFlow = cacheManager.cacheStatusForEpisode(subjectId, episode.id)
+                    val cacheStatusFlow = cacheManager.cacheStatusForEpisode(subjectId, episode.id)
 
-            EpisodeCacheState(
-                episodeId = episode.id,
-                cacheRequesterLazy = {
-                    EpisodeCacheRequester(
+                    val cacheRequester = EpisodeCacheRequester(
                         mediaSourceManager.mediaFetcher,
                         MediaSelectorFactory.withKoin(),
                         storagesLazy = cacheManager.enabledStorages,
                     )
+                    EpisodeCacheState(
+                        episodeId = episode.id,
+                        cacheRequester = cacheRequester,
+                        currentStageState = cacheRequester.stage.produceState(scope = this),
+                        infoState = stateOf(
+                            EpisodeCacheInfo(
+                                sort = episode.sort,
+                                ep = episode.ep,
+                                title = episode.displayName,
+                                watchStatus = episodeCollection.collectionType,
+                                hasPublished = episode.isKnownCompleted,
+                            ),
+                        ),
+                        cacheStatusState = cacheStatusFlow.produceState(null, this),
+                        backgroundScope = this,
+                    )
                 },
-                info = MutableStateFlow(
-                    EpisodeCacheInfo(
-                        sort = episode.sort,
-                        ep = episode.ep,
-                        title = episode.displayName,
-                        watchStatus = episodeCollection.collectionType,
-                        hasPublished = episode.isKnownCompleted,
-                    ),
-                ),
-                cacheStatusFlow = cacheStatusFlow,
-                parentCoroutineContext = backgroundScope.coroutineContext,
             )
         }
     }.shareInBackground()
@@ -129,6 +130,17 @@ class SubjectCacheViewModelImpl(
      */
     override val cacheListState: EpisodeCacheListState = EpisodeCacheListStateImpl(
         episodes = episodesFlow.produceState(emptyList()),
+        currentEpisode = episodesFlow.flatMapLatest { episodes ->
+            combine(
+                episodes.map { episodeCacheState ->
+                    episodeCacheState.cacheRequester.stage.map { episodeCacheState to it }
+                },
+            ) { results ->
+                results.firstOrNull { (_, stage) ->
+                    stage is CacheRequestStage.Working
+                }?.first
+            }
+        }.produceState(null),
         onRequestCache = { episode, autoSelectByCached ->
             episode.cacheRequester.request(
                 EpisodeCacheRequest(
@@ -152,7 +164,6 @@ class SubjectCacheViewModelImpl(
                 it.metadata.episodeId == episodeId
             }
         },
-        backgroundScope.coroutineContext,
     )
 
     init {

app/shared/src/desktopTest/kotlin/ui/subject/cache/EpisodeCacheStateTest.kt
@@ -1,22 +1,20 @@
-@file:OptIn(ExperimentalStdlibApi::class)
-
 package me.him188.ani.app.ui.subject.cache
 
 import androidx.compose.runtime.snapshots.Snapshot
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import me.him188.ani.app.data.models.episode.EpisodeInfo
 import me.him188.ani.app.data.models.subject.SubjectInfo
 import me.him188.ani.app.data.source.media.TestMediaList
 import me.him188.ani.app.data.source.media.cache.EpisodeCacheStatus
+import me.him188.ani.app.data.source.media.cache.requester.CacheRequestStage
 import me.him188.ani.app.data.source.media.cache.requester.EpisodeCacheRequest
 import me.him188.ani.app.data.source.media.cache.requester.EpisodeCacheRequester
 import me.him188.ani.app.data.source.media.cache.requester.trySelectSingle
@@ -27,6 +25,7 @@ import me.him188.ani.app.data.source.media.framework.TestMediaSelector
 import me.him188.ani.app.data.source.media.instance.createTestMediaSourceInstance
 import me.him188.ani.app.data.source.media.selector.MediaSelector
 import me.him188.ani.app.data.source.media.selector.MediaSelectorFactory
+import me.him188.ani.app.ui.foundation.produceState
 import me.him188.ani.app.ui.framework.runComposeStateTest
 import me.him188.ani.app.ui.framework.takeSnapshot
 import me.him188.ani.datasources.api.EpisodeSort
@@ -62,14 +61,18 @@ class EpisodeCacheStateTest {
         EpisodeCacheStatus.Cached(300.megaBytes),
     )
 
-    private suspend fun TestScope.createEpisodeCacheState() = TestEpisodeCacheState(
-        1,
-        cacheRequesterLazy = { createTestEpisodeCacheRequester() },
-        infoFlow = infoFlow,
-        cacheStatusFlow = cacheStatusFlow,
-        parentCoroutineContext = currentCoroutineContext(),
-    ).apply {
-        testScheduler.runCurrent() // start `launch` and `produceState`
+    private fun TestScope.createEpisodeCacheState(): EpisodeCacheState {
+        val cacheRequester = createTestEpisodeCacheRequester()
+        return EpisodeCacheState(
+            1,
+            cacheRequester = cacheRequester,
+            currentStageState = cacheRequester.stage.produceState(scope = backgroundScope),
+            infoState = infoFlow.produceState(scope = backgroundScope),
+            cacheStatusState = cacheStatusFlow.produceState(scope = backgroundScope),
+            backgroundScope = backgroundScope,
+        ).apply {
+            testScheduler.runCurrent() // start `launch` and `produceState`
+        }
     }
 
     private fun createTestEpisodeCacheRequester(): EpisodeCacheRequester {
@@ -110,14 +113,11 @@ class EpisodeCacheStateTest {
 
         assertEquals(infoFlow.value, state.info)
         assertEquals(false, state.isInfoLoading)
-        assertEquals(null, state.currentSelectMediaTask)
-        assertEquals(null, state.currentSelectStorageTask)
+        assertEquals(CacheRequestStage.Idle, state.currentStage)
         assertIs<EpisodeCacheStatus.Cached>(state.cacheStatus)
         assertEquals(true, state.canCache)
         assertEquals(false, state.actionTasker.isRunning)
         assertEquals(false, state.showProgressIndicator)
-
-        state.backgroundScope.cancel()
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -129,7 +129,6 @@ class EpisodeCacheStateTest {
         infoFlow.value = infoFlow.value.copy(watchStatus = UnifiedCollectionType.WISH)
         val state = createEpisodeCacheState()
         assertEquals(true, state.canCache)
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -137,7 +136,6 @@ class EpisodeCacheStateTest {
         infoFlow.value = infoFlow.value.copy(watchStatus = UnifiedCollectionType.DOING)
         val state = createEpisodeCacheState()
         assertEquals(true, state.canCache)
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -145,7 +143,6 @@ class EpisodeCacheStateTest {
         infoFlow.value = infoFlow.value.copy(watchStatus = UnifiedCollectionType.ON_HOLD)
         val state = createEpisodeCacheState()
         assertEquals(true, state.canCache)
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -153,7 +150,6 @@ class EpisodeCacheStateTest {
         infoFlow.value = infoFlow.value.copy(watchStatus = UnifiedCollectionType.NOT_COLLECTED)
         val state = createEpisodeCacheState()
         assertEquals(true, state.canCache)
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -161,7 +157,6 @@ class EpisodeCacheStateTest {
         infoFlow.value = infoFlow.value.copy(watchStatus = UnifiedCollectionType.DONE)
         val state = createEpisodeCacheState()
         assertEquals(false, state.canCache)
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -169,7 +164,6 @@ class EpisodeCacheStateTest {
         infoFlow.value = infoFlow.value.copy(watchStatus = UnifiedCollectionType.DROPPED)
         val state = createEpisodeCacheState()
         assertEquals(false, state.canCache)
-        state.backgroundScope.cancel()
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -180,7 +174,6 @@ class EpisodeCacheStateTest {
     fun `showProgressIndicator is initially false`() = runComposeStateTest {
         val state = createEpisodeCacheState()
         assertEquals(false, state.showProgressIndicator)
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -198,7 +191,6 @@ class EpisodeCacheStateTest {
 
         takeSnapshot()
         assertEquals(false, state.showProgressIndicator)
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -209,7 +201,6 @@ class EpisodeCacheStateTest {
 
         takeSnapshot()
         assertEquals(true, state.showProgressIndicator)
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -221,7 +212,6 @@ class EpisodeCacheStateTest {
 
         takeSnapshot()
 
-        state.backgroundScope.cancel()
     }
 
     @Test
@@ -235,7 +225,6 @@ class EpisodeCacheStateTest {
         takeSnapshot()
 
         assertEquals(false, state.showProgressIndicator)
-        state.backgroundScope.cancel()
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -245,8 +234,7 @@ class EpisodeCacheStateTest {
     @Test
     fun `currentSelectMediaTask is null when no task`() = runComposeStateTest {
         val state = createEpisodeCacheState()
-        assertEquals(null, state.currentSelectMediaTask)
-        state.backgroundScope.cancel()
+        assertEquals(CacheRequestStage.Idle, state.currentStage)
     }
 
     @Test
@@ -257,25 +245,11 @@ class EpisodeCacheStateTest {
 
         takeSnapshot()
 
-        assertEquals(true, state.currentSelectMediaTask != null)
-        state.backgroundScope.cancel()
+        assertIs<CacheRequestStage.SelectMedia>(state.currentStage)
     }
 
     @Test
-    fun `currentSelectMediaTask is not null when SelectStorage`() = runComposeStateTest {
-        val state = createEpisodeCacheState()
-        state.cacheRequester
-            .request(EpisodeCacheRequest(SubjectInfo.Empty, EpisodeInfo(1)))
-            .select(TestMediaList[0])
-
-        takeSnapshot()
-
-        assertEquals(true, state.currentSelectMediaTask != null)
-        state.backgroundScope.cancel()
-    }
-
-    @Test
-    fun `currentSelectMediaTask is null when Done`() = runComposeStateTest {
+    fun `currentStage is Done when Done`() = runComposeStateTest {
         val state = createEpisodeCacheState()
         state.cacheRequester
             .request(EpisodeCacheRequest(SubjectInfo.Empty, EpisodeInfo(1)))
@@ -283,8 +257,8 @@ class EpisodeCacheStateTest {
             .trySelectSingle()
 
         takeSnapshot()
+        runCurrent()
 
-        assertEquals(null, state.currentSelectMediaTask)
-        state.backgroundScope.cancel()
+        assertIs<CacheRequestStage.Done>(state.currentStage)
     }
 }

commit b4917d038ac9dfb934df8247a8a0e6a8391813a3

Fix CacheListGroup previews
caused by 9f5dc62b59665ee243e878c9b276e8cb8336d0db

app/shared/src/androidMain/kotlin/ui/subject/cache/CacheListGroup.android.kt
@@ -17,16 +17,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.flowOf
 import me.him188.ani.app.data.source.media.cache.EpisodeCacheStatus
+import me.him188.ani.app.data.source.media.cache.requester.CacheRequestStage
 import me.him188.ani.app.data.source.media.cache.requester.EpisodeCacheRequester
 import me.him188.ani.app.data.source.media.selector.MediaSelectorFactory
 import me.him188.ani.app.ui.foundation.ProvideCompositionLocalsForPreview
+import me.him188.ani.app.ui.foundation.stateOf
 import me.him188.ani.app.ui.settings.SettingsTab
 import me.him188.ani.datasources.api.EpisodeSort
 import me.him188.ani.datasources.api.topic.UnifiedCollectionType
-import kotlin.coroutines.EmptyCoroutineContext
+import me.him188.ani.utils.platform.annotations.TestOnly
 
 @Composable
 @Preview
@@ -121,6 +124,7 @@ private fun PreviewEpisodeCacheActionIconHasActionRunningChange() {
     }
 }
 
+@OptIn(TestOnly::class)
 @Preview
 @Composable
 private fun PreviewEpisodeItem() = ProvideCompositionLocalsForPreview {
@@ -129,23 +133,15 @@ private fun PreviewEpisodeItem() = ProvideCompositionLocalsForPreview {
         listOf(true, false).forEach { hasPublished ->
             UnifiedCollectionType.entries.forEach { watchStatus ->
                 EpisodeCacheItem(
-                    episode = remember {
-                        EpisodeCacheState(
-                            0,
-                            { EpisodeCacheRequester(flowOf(), MediaSelectorFactory.withKoin(), flowOf()) },
-                            infoState = MutableStateFlow(
-                                EpisodeCacheInfo(
-                                    sort = EpisodeSort(++id),
-                                    ep = null,
-                                    title = "$watchStatus - ${if (hasPublished) "已开播" else "未开播"}",
-                                    watchStatus = watchStatus,
-                                    hasPublished = true,
-                                ),
-                            ),
-                            cacheStatusState = MutableStateFlow(EpisodeCacheStatus.NotCached),
-                            EmptyCoroutineContext,
-                        )
-                    },
+                    episode = rememberTestEpisodeCacheState(
+                        info = EpisodeCacheInfo(
+                            sort = EpisodeSort(++id),
+                            ep = null,
+                            title = "$watchStatus - ${if (hasPublished) "已开播" else "未开播"}",
+                            watchStatus = watchStatus,
+                            hasPublished = true,
+                        ),
+                    ),
                     onClick = {},
                     isRequestHidden = false,
                     dropdown = { },
@@ -155,31 +151,42 @@ private fun PreviewEpisodeItem() = ProvideCompositionLocalsForPreview {
     }
 }
 
+@OptIn(TestOnly::class)
 @Preview
 @Composable
 private fun PreviewEpisodeItemVeryLong() = ProvideCompositionLocalsForPreview {
     SettingsTab {
         EpisodeCacheItem(
-            remember {
-                EpisodeCacheState(
-                    0,
-                    { EpisodeCacheRequester(flowOf(), MediaSelectorFactory.withKoin(), flowOf()) },
-                    infoState = MutableStateFlow(
-                        EpisodeCacheInfo(
-                            sort = EpisodeSort(1),
-                            ep = null,
-                            title = "测试标题".repeat(10),
-                            watchStatus = UnifiedCollectionType.WISH,
-                            hasPublished = true,
-                        ),
-                    ),
-                    cacheStatusState = MutableStateFlow(EpisodeCacheStatus.NotCached),
-                    EmptyCoroutineContext,
-                )
-            },
+            rememberTestEpisodeCacheState(
+                EpisodeCacheInfo(
+                    sort = EpisodeSort(1),
+                    ep = null,
+                    title = "测试标题".repeat(10),
+                    watchStatus = UnifiedCollectionType.WISH,
+                    hasPublished = true,
+                ),
+                EpisodeCacheStatus.NotCached,
+            ),
             onClick = {},
             isRequestHidden = false,
             dropdown = { },
         )
     }
+}
+
+@Composable
+@OptIn(DelicateCoroutinesApi::class)
+@TestOnly
+fun rememberTestEpisodeCacheState(
+    info: EpisodeCacheInfo,
+    cacheStatus: EpisodeCacheStatus = EpisodeCacheStatus.NotCached,
+) = remember {
+    EpisodeCacheState(
+        0,
+        EpisodeCacheRequester(flowOf(), MediaSelectorFactory.withKoin(), flowOf()),
+        currentStageState = stateOf(CacheRequestStage.Idle),
+        infoState = stateOf(info),
+        cacheStatusState = stateOf(cacheStatus),
+        GlobalScope,
+    )
 }