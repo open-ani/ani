50858b6fe1b2429aef47dc0811b7cd69bd9bdf0f
Him188 <Him188@mamoe.net>
2024-08-28 22:04:24 +0100

    Do not link ios framework as static library
    to hopefully improve build times

--- app/shared/build.gradle.kts
+++ app/shared/build.gradle.kts
@@ -61,7 +61,7 @@ kotlin {
         ).forEach { iosTarget ->
             iosTarget.binaries.framework {
                 baseName = "ComposeApp"
-                isStatic = true
+                isStatic = false
             }
         }
     }
