bd18c2eac34edf127d89e25919f99a59445e96a8
Him188 <Him188@mamoe.net>
2024-08-31 09:00:22 +0100

    Build only arm64-v8a android on macos aarch64 CI

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -50,6 +50,16 @@ dependencies {
     implementation(libs.ktor.client.core)
 }
 
+val archs = buildList {
+    if (project.hasProperty("ani.android.abis")) {
+        addAll(project.property("ani.android.abis").toString().split(",").map { it.trim() })
+    } else {
+        add("arm64-v8a")
+        add("armeabi-v7a")
+        add("x86_64")
+    }
+}
+
 android {
     namespace = "me.him188.ani.android"
     compileSdk = getIntProperty("android.compile.sdk")
@@ -63,14 +73,16 @@ android {
             // Specifies the ABI configurations of your native
             // libraries Gradle should build and package with your app.
             abiFilters.clear()
-            abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
+            //noinspection ChromeOsAbiSupport
+            abiFilters += archs
         }
     }
     splits {
         abi {
             isEnable = true
             reset()
-            include("arm64-v8a", "armeabi-v7a", "x86_64")
+            //noinspection ChromeOsAbiSupport
+            include(*archs.toTypedArray())
             isUniversalApk = true // 额外构建一个
         }
     }
