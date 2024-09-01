248445552efedcb968601a80e4181463228f321e
Him188 <Him188@mamoe.net>
2024-09-01 12:54:51 +0100

    Fix ui tests

--- app/shared/src/jvmTest/kotlin/ui/framework/UiTest.jvm.kt
+++ app/shared/src/jvmTest/kotlin/ui/framework/UiTest.jvm.kt
@@ -1,10 +1,13 @@
 package me.him188.ani.app.ui.framework
 
+import androidx.compose.ui.test.ComposeUiTest
 import androidx.compose.ui.test.runComposeUiTest
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.resetMain
 import kotlin.time.Duration.Companion.minutes
 
 /**
@@ -12,7 +15,12 @@ import kotlin.time.Duration.Companion.minutes
  * - [ComposeUiTest.waitUntil] 的超时时间更长
  */
 @OptIn(DelicateCoroutinesApi::class)
-actual fun runAniComposeUiTest(testBody: AniComposeUiTest.() -> Unit) = runComposeUiTest {
+actual fun runAniComposeUiTest(testBody: AniComposeUiTest.() -> Unit) {
+    // 一定要调用这个, 否则在跟其他协程测试一起跑的时候, lifecycle 的一个地方会一直 block
+    // androidx.lifecycle.MainDispatcherChecker.updateMainDispatcherThread
+    Dispatchers.resetMain()
+
+
     val testThread = Thread.currentThread()
     var timedOut = false
     val job = GlobalScope.launch {
@@ -22,14 +30,16 @@ actual fun runAniComposeUiTest(testBody: AniComposeUiTest.() -> Unit) = runCompo
     }
 
     try {
-        AniComposeUiTestImpl(this).run(testBody)
+        return runComposeUiTest {
+            AniComposeUiTestImpl(this).run(testBody)
+        }
     } catch (e: InterruptedException) {
         if (timedOut) {
             throw AssertionError("Test timed out after 1 minute")
         } else {
             throw e
         }
+    } finally {
+        job.cancel()
     }
-
-    job.cancel()
 }
