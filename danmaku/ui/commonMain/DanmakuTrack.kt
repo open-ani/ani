7e6e3b160a06c26736d9e3c6ee419312832c6796
StageGuard <sg@mamoe.net>
2024-09-02 18:03:57 +0800

    update doc of `DanmakuTrack`

--- danmaku/ui/commonMain/DanmakuTrack.kt
+++ danmaku/ui/commonMain/DanmakuTrack.kt
@@ -10,19 +10,25 @@ import androidx.compose.runtime.Stable
 @Stable
 interface DanmakuTrack<T : SizeSpecifiedDanmaku, D> {
     /**
-     * place a danmaku to the track without check.
+     * 放置弹幕到此轨道, 无状态检查
      *
-     * @return A positioned danmaku which can be placed on danmaku host.
+     * @return 返回已经放置的弹幕
      */
     fun place(danmaku: T, placeTimeNanos: Long = NOT_PLACED): D
 
     /**
-     * check if this track can place danmaku.
+     * 检测这条弹幕是否可以放置到此轨道中.
+     * 
+     * 目前的行为:
+     * - [浮动弹幕][FloatingDanmakuTrack]: 如果放置后不会和已有弹幕重叠并且[放置时间][placeTimeNanos]对应的[位置][FloatingDanmaku.distanceX]不在轨道右侧外.
+     * - [固定弹幕][FixedDanmakuTrack] : 当前没有正在显示的弹幕并且[放置时间][placeTimeNanos]距离当前时间在[显示时间][FixedDanmakuTrack.durationMillis]内.
      */
     fun canPlace(danmaku: T, placeTimeNanos: Long = NOT_PLACED): Boolean
 
     /**
-     * try to place a danmaku. there are reasons that the upcoming danmaku cannot be placed.
+     * 尝试放置弹幕
+     * 
+     * @return 无法放置返回 `null`, 可放置则返回已放置的弹幕.
      */
     fun tryPlace(
         danmaku: T, 
@@ -33,14 +39,22 @@ interface DanmakuTrack<T : SizeSpecifiedDanmaku, D> {
     }
 
     /**
-     * clear all danmaku in this track
+     * 清除当前轨道里的所有弹幕
      */
     fun clearAll()
 
     /**
-     * check for visibility of danmaku in this track at logical tick
+     * 需要循环执行的逻辑帧.
+     * 
+     * 弹幕轨道上述的方法通常依赖时间来进行判断, 可执行此逻辑帧 tick 来进行判断.
+     * 
+     * 如果不需要判断则不需要实现此方法.
+     * 
+     * 目前的 [FloatingDanmakuTrack] 和 [FixedDanmakuTrack] 均实现了逻辑帧并进行以下行为:
+     * - 基于帧时间判断是否需要移除轨道中的过期弹幕.
+     * - 基于已有弹幕判断当前时间点是否可以放置一条新弹幕.
      */
-    fun tick()
+    fun tick() { }
     
     companion object {
         const val NOT_PLACED = -1L
