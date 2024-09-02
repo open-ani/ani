07786e377280ffa5648f38488169903b39a4e4f2
Him188 <Him188@mamoe.net>
2024-09-02 23:27:09 +0100

    Reformat DanmakuHostState

--- danmaku/ui/commonMain/DanmakuHostState.kt
+++ danmaku/ui/commonMain/DanmakuHostState.kt
@@ -34,6 +34,7 @@ class DanmakuHostState(
 ) {
     private val danmakuConfig by danmakuConfigState
     private val uiContext: UIContext = UIContext()
+
     /**
      * DanmakuHost 显示大小, 在显示时修改
      */
@@ -45,16 +46,18 @@ class DanmakuHostState(
     private val trackHeightState = mutableIntStateOf(0)
     internal var trackHeight by trackHeightState
         private set
+
     // currently not configurable
     // private val floatingSpeedMultiplierState = mutableFloatStateOf(danmakuTrackProperties.speedMultiplier)
     // currently not configurable
     private val fixedDanmakuPresentDuration = mutableLongStateOf(danmakuTrackProperties.fixedDanmakuPresentDuration)
-    
+
     internal val canvasAlpha by derivedStateOf { danmakuConfig.style.alpha }
     internal var paused by mutableStateOf(false)
     internal val isDebug by derivedStateOf { danmakuConfig.isDebug }
-    
+
     private val elapsedFrameTimeNanoState = mutableLongStateOf(0)
+
     /**
      * 已经过的帧时间, 在 [setPaused] 设置暂停时此帧时间也会暂停
      */
@@ -66,7 +69,7 @@ class DanmakuHostState(
     internal val floatingTrack = mutableListOf<FloatingDanmakuTrack<StyledDanmaku>>()
     internal val topTrack = mutableListOf<FixedDanmakuTrack<StyledDanmaku>>()
     internal val bottomTrack = mutableListOf<FixedDanmakuTrack<StyledDanmaku>>()
-    
+
     /**
      * 所有在 [floatingTrack], [topTrack] 和 [bottomTrack] 弹幕.
      * 在这里保留一个引用, 方便在 [recalculatePresentDanmakuPositions] 的时候重新计算所有弹幕位置.
@@ -74,7 +77,7 @@ class DanmakuHostState(
      */
     internal val presentFloatingDanmaku: MutableList<FloatingDanmaku<StyledDanmaku>> = mutableListOf()
     internal val presentFixedDanmaku: MutableList<FixedDanmaku<StyledDanmaku>> = mutableListOf()
-    
+
     fun setUIContext(
         baseStyle: TextStyle,
         textMeasurer: TextMeasurer,
@@ -82,7 +85,7 @@ class DanmakuHostState(
     ) {
         uiContext.set(baseStyle, textMeasurer, density)
     }
-    
+
     /**
      * 监听 [DanmakuConfig] 和 [hostHeight] 配置变化
      */
@@ -101,7 +104,7 @@ class DanmakuHostState(
                 var lastFontSize = danmakuConfig.style.fontSize
                 // var lastSafeSeparation = danmakuConfig.safeSeparation
                 // var lastIsDebug = danmakuConfig.isDebug
-                
+
                 combine(
                     snapshotFlow { hostHeight },
                     snapshotFlow { danmakuConfig }.distinctUntilChanged { old, new ->
@@ -110,24 +113,24 @@ class DanmakuHostState(
                                 && old.enableTop == new.enableTop
                                 && old.enableFloating == new.enableFloating
                                 && old.enableBottom == new.enableBottom
-                        
-                    }
+
+                    },
                 ) { newHeight, newConfig ->
                     val dummyTextLayout = dummyDanmaku(
                         measurer,
-                        uiContext.baseStyle, 
-                        newConfig.style, 
-                        "哈哈哈哈"
+                        uiContext.baseStyle,
+                        newConfig.style,
+                        "哈哈哈哈",
                     ).solidTextLayout
-                    val verticalPadding = with(uiContext.density) { 
-                        (danmakuTrackProperties.verticalPadding * 2).dp.toPx() 
+                    val verticalPadding = with(uiContext.density) {
+                        (danmakuTrackProperties.verticalPadding * 2).dp.toPx()
                     }
 
                     val trackHeight = (dummyTextLayout.size.height + verticalPadding).toInt()
                     val trackCount = floor(newHeight / trackHeight * newConfig.displayArea)
                         .coerceAtLeast(1f)
                         .toInt()
-                    
+
                     Triple(trackCount, trackHeight, newConfig)
                 }
                     .distinctUntilChanged()
@@ -142,7 +145,7 @@ class DanmakuHostState(
                             recalculatePresentDanmakuPositions(elapsedFrameTimeNanos)
                             lastFontSize = newConfig.style.fontSize
                         }
-                        danmakuUpdateSubscription ++ // update subscription manually if paused
+                        danmakuUpdateSubscription++ // update subscription manually if paused
                     }
             }
             /**
@@ -154,10 +157,10 @@ class DanmakuHostState(
             launch {
                 snapshotFlow { danmakuConfig }.distinctUntilChanged { old, new ->
                     old.safeSeparation == new.safeSeparation && old.isDebug == new.isDebug
-                }.collect { newConfig -> 
+                }.collect { newConfig ->
                     updateTrackProperties(newConfig)
                     recalculatePresentDanmakuPositions(elapsedFrameTimeNanos)
-                    danmakuUpdateSubscription ++ // update subscription manually if paused
+                    danmakuUpdateSubscription++ // update subscription manually if paused
                 }
             }
             /**
@@ -170,12 +173,12 @@ class DanmakuHostState(
                     old.speed == new.speed
                 }.collect { newConfig ->
                     updateTrackProperties(newConfig)
-                    danmakuUpdateSubscription ++ // update subscription manually if paused
+                    danmakuUpdateSubscription++ // update subscription manually if paused
                 }
             }
             /**
              * 以下变化将仅导致弹幕重新变更样式, 而不用重新计算静态位置
-             * 
+             *
              * - [DanmakuStyle.alpha] in [DanmakuConfig.style]
              * - [DanmakuStyle.fontWeight] in [DanmakuConfig.style]
              * - [DanmakuStyle.shadow] in [DanmakuConfig.style]
@@ -197,25 +200,25 @@ class DanmakuHostState(
                             return copy(
                                 baseStyle = uiContext.baseStyle,
                                 style = newConfig.style,
-                                enableColor = newConfig.enableColor
+                                enableColor = newConfig.enableColor,
                             )
                         }
-                        
+
                         for (danmaku in presentFloatingDanmaku) danmaku.danmaku = danmaku.danmaku.transform()
                         for (danmaku in presentFixedDanmaku) danmaku.danmaku = danmaku.danmaku.transform()
-                        danmakuUpdateSubscription ++ // update subscription manually if paused
+                        danmakuUpdateSubscription++ // update subscription manually if paused
                     }
             }
             /**
              * 如果在暂停的时候屏幕高度和宽度有变化, 需要更新一次固定弹幕的位置
              */
-            launch { 
+            launch {
                 snapshotFlow { hostHeight }.collect {
                     if (!paused) return@collect
                     for (danmaku in presentFixedDanmaku) {
                         if (danmaku.fromBottom) danmaku.y = danmaku.calculatePosY()
                     }
-                    danmakuUpdateSubscription ++ // update subscription manually
+                    danmakuUpdateSubscription++ // update subscription manually
                 }
             }
         }
@@ -235,12 +238,12 @@ class DanmakuHostState(
                 trackIndex = index,
                 frameTimeNanosState = elapsedFrameTimeNanoState,
                 trackHeight = trackHeightState,
-                trackWidth = hostWidthState, 
+                trackWidth = hostWidthState,
                 speedPxPerSecond = newFloatingTrackSpeed,
                 safeSeparation = newFloatingTrackSafeSeparation,
                 // baseTextLength = floatingBaseTextLengthForSpeed,
                 // speedMultiplier = floatingSpeedMultiplierState,
-                onRemoveDanmaku = { removed -> presentFloatingDanmaku.removeFirst { it.danmaku == removed.danmaku } }
+                onRemoveDanmaku = { removed -> presentFloatingDanmaku.removeFirst { it.danmaku == removed.danmaku } },
             )
         }
         topTrack.setTrackCountImpl(if (config.enableTop) count else 0) { index ->
@@ -252,7 +255,7 @@ class DanmakuHostState(
                 fromBottom = false,
                 durationMillis = fixedDanmakuPresentDuration,
                 onTickReplacePending = { new -> presentFixedDanmaku.add(new) },
-                onRemoveDanmaku = { removed -> presentFixedDanmaku.removeFirst { it.danmaku == removed.danmaku } }
+                onRemoveDanmaku = { removed -> presentFixedDanmaku.removeFirst { it.danmaku == removed.danmaku } },
             )
         }
         bottomTrack.setTrackCountImpl(if (config.enableBottom) count else 0) { index ->
@@ -264,7 +267,7 @@ class DanmakuHostState(
                 fromBottom = true,
                 durationMillis = fixedDanmakuPresentDuration,
                 onTickReplacePending = { new -> presentFixedDanmaku.add(new) },
-                onRemoveDanmaku = { removed -> presentFixedDanmaku.removeFirst { it.danmaku == removed.danmaku } }
+                onRemoveDanmaku = { removed -> presentFixedDanmaku.removeFirst { it.danmaku == removed.danmaku } },
             )
         }
     }
@@ -281,7 +284,7 @@ class DanmakuHostState(
         val newFloatingTrackSpeed = with(uiContext.density) { config.speed.dp.toPx() }
         val newFloatingTrackSafeSeparation = with(uiContext.density) { config.safeSeparation.toPx() }
 
-        floatingTrack.forEach { 
+        floatingTrack.forEach {
             it.safeSeparation = newFloatingTrackSafeSeparation
             it.speedPxPerSecond = newFloatingTrackSpeed
         }
@@ -289,7 +292,7 @@ class DanmakuHostState(
 
     /**
      * 重新放置屏幕上弹幕的位置. 这也会导致样式和静态位置重置.
-     * 
+     *
      * 此方法的行为与 [repopulate] 相同, 但是具有更高的执行效率.
      */
     @UiThread
@@ -297,14 +300,14 @@ class DanmakuHostState(
         uiContext.await()
         val presentFloatingDanmakuCopied = presentFloatingDanmaku.toList()
         val presentFixedDanmakuCopied = presentFixedDanmaku.toList()
-        
+
         val floatingTrackSpeed = with(uiContext.density) { danmakuConfig.speed.dp.toPx() }
-        
+
         clearPresentDanmaku()
-        
+
         presentFixedDanmakuCopied.forEach { trySend(it.danmaku.presentation, it.placeFrameTimeNanos) }
-        presentFloatingDanmakuCopied.forEach { 
-            val placeFrameTimeNanos = currentElapsedFrameTimeNanos - 
+        presentFloatingDanmakuCopied.forEach {
+            val placeFrameTimeNanos = currentElapsedFrameTimeNanos -
                     ((it.distanceX / floatingTrackSpeed) * 1_000_000_000f).toLong()
             if (placeFrameTimeNanos >= 0) {
                 trySend(it.danmaku.presentation, placeFrameTimeNanos)
@@ -315,15 +318,15 @@ class DanmakuHostState(
     @UiThread
     internal suspend fun interpolateFrameLoop() {
         uiContext.await()
-        coroutineScope { 
+        coroutineScope {
             var currentFloatingTrackSpeed = with(uiContext.density) { danmakuConfig.speed.dp.toPx() }
-            
-            launch { 
-                snapshotFlow { danmakuConfig.speed }.collect { 
+
+            launch {
+                snapshotFlow { danmakuConfig.speed }.collect {
                     currentFloatingTrackSpeed = with(uiContext.density) { danmakuConfig.speed.dp.toPx() }
                 }
             }
-            
+
             launch {
                 var currentFrameTimeNanos = withFrameNanos {
                     // 使用了这一帧来获取时间, 需要补偿平均帧时间
@@ -352,8 +355,8 @@ class DanmakuHostState(
                             // calculate y once
                             if (danmaku.y.isNaN()) danmaku.y = danmaku.calculatePosY()
                         }
-                        
-                        danmakuUpdateSubscription ++ // update subscription manually if paused
+
+                        danmakuUpdateSubscription++ // update subscription manually if paused
                     }
                 }
             }
@@ -372,17 +375,17 @@ class DanmakuHostState(
 
     /**
      * 尝试发送弹幕到屏幕, 如果当前时间点已没有更多轨道可以使用则会发送失败.
-     * 
+     *
      * 对于一定发送成功的版本, 请查看 [DanmakuHostState.send].
-     * 
+     *
      * @return 如果发送成功则返回 true
      * @see DanmakuHostState.send
      */
     // 若是浮动弹幕则加入到 [presentFloatingDanmaku], 固定弹幕加到 [presentFixedDanmaku].
     suspend fun trySend(
-        danmaku: DanmakuPresentation, 
+        danmaku: DanmakuPresentation,
         placeFrameTimeNanos: Long = DanmakuTrack.NOT_PLACED
-    ) : Boolean {
+    ): Boolean {
         uiContext.await()
         val styledDanmaku = StyledDanmaku(
             presentation = danmaku,
@@ -392,21 +395,23 @@ class DanmakuHostState(
             enableColor = danmakuConfig.enableColor,
             isDebug = danmakuConfig.isDebug,
         )
-        
+
         return withContext(Dispatchers.Main.immediate) {
             when (danmaku.danmaku.location) {
                 DanmakuLocation.NORMAL -> {
-                    val floatingDanmaku = floatingTrack.firstNotNullOfOrNull { 
+                    val floatingDanmaku = floatingTrack.firstNotNullOfOrNull {
                         it.tryPlace(styledDanmaku, placeFrameTimeNanos)
                     }
                     floatingDanmaku?.also(presentFloatingDanmaku::add) != null
                 }
+
                 DanmakuLocation.TOP -> {
                     val floatingDanmaku = topTrack.firstNotNullOfOrNull {
                         it.tryPlace(styledDanmaku, placeFrameTimeNanos)
                     }
                     floatingDanmaku?.also(presentFixedDanmaku::add) != null
                 }
+
                 DanmakuLocation.BOTTOM -> {
                     val floatingDanmaku = bottomTrack.firstNotNullOfOrNull {
                         it.tryPlace(styledDanmaku, placeFrameTimeNanos)
@@ -444,7 +449,7 @@ class DanmakuHostState(
                     val lastDanmaku = track.getLastDanmaku() ?: kotlin.run {
                         // 表示当前轨道没有弹幕, 可以直接发送到这条轨道
                         track.place(styledDanmaku).also(presentFloatingDanmaku::add)
-                        return@withContext 
+                        return@withContext
                     }
                     val right = lastDanmaku.distanceX - (lastDanmaku.danmaku.danmakuWidth + safeSeparation)
                     if (right >= 0) {
@@ -505,38 +510,38 @@ class DanmakuHostState(
 
     /**
      * 清空屏幕并填充 [list] 到屏幕.
-     * 
+     *
      * **此方法不会重新排序 [list], 所以弹幕必须已经按发送时间排序**.
-     * 
+     *
      * **在通常情况下, 此方法假设 [list] 中弹幕是在 repopulate 时的[帧时间][elapsedFrameTimeNanos]之前发送的弹幕**.
-     * 因此, [list] 最后一条浮动弹幕会被放到屏幕的最右侧. 
+     * 因此, [list] 最后一条浮动弹幕会被放到屏幕的最右侧.
      * 其他弹幕将以其[发送时间戳][Danmaku.playTimeMillis]为基准依次**向前**排列.
-     * 
+     *
      * 但是如果 [list] 中的第一条浮动弹幕是可以显示在最开始 trackDurationMillis 内的时间
      * (trackDurationMillis 表示一条浮动的那幕从屏幕最右侧滚动到最左侧的时间),
      * 那么 [list] 中的第一条弹幕会被放到屏幕对应位置来模拟最开始的弹幕滚动过程,
      * 其他弹幕将以其[发送时间戳][Danmaku.playTimeMillis]为基准依次**向后**排列.
-     * 
+     *
      * 例如 trackDurationMillis 为 `10000ms`, [list] 中的第一条弹幕的发送时间为 `5000ms`,
      * 那么第一条弹幕就会被放置在轨道的中间位置.
-     * 
+     *
      * 通过设置 [timeOffsetMillis] 来指定整体的弹幕放置时间偏移.
      * 若 `timeOffsetMillis < 0L` 则弹幕放置的位置会向左偏移 [timeOffsetMillis] 的[帧时间][elapsedFrameTimeNanos].
-     * 
+     *
      * @param timeOffsetMillis 弹幕放置偏移
      * @param list 要填充到屏幕的弹幕, 必须按发送时间戳排序.
      */
     suspend fun repopulate(list: List<DanmakuPresentation>, timeOffsetMillis: Long = 0L) {
         if (list.isEmpty()) return
         uiContext.await()
-        
+
         withContext(Dispatchers.Main.immediate) { clearPresentDanmaku() }
         val currentElapsedFrameTimeNanos = elapsedFrameTimeNanos // take snapshot
-        
-        val isFloatingDanmaku = { danmaku: DanmakuPresentation -> 
+
+        val isFloatingDanmaku = { danmaku: DanmakuPresentation ->
             danmaku.danmaku.location == DanmakuLocation.NORMAL
         }
-        
+
         val floatingDanmaku = list.filter(isFloatingDanmaku)
         if (floatingDanmaku.isNotEmpty()) {
             // 第一条和最后一条浮动弹幕发送时间戳
@@ -562,7 +567,7 @@ class DanmakuHostState(
                 if (playFrameTimeNanos >= 0) trySend(danmaku, playFrameTimeNanos)
             }
         }
-        
+
         val fixedDanmaku = list.filterNot(isFloatingDanmaku)
         if (fixedDanmaku.isNotEmpty()) {
             val lastDanmakuTimeMillis = fixedDanmaku.last().danmaku.playTimeMillis
@@ -574,7 +579,7 @@ class DanmakuHostState(
             }
         }
     }
-    
+
     @UiThread
     private fun clearPresentDanmaku() {
         floatingTrack.forEach { it.clearAll() }
@@ -588,24 +593,24 @@ class DanmakuHostState(
             "presentFloatingDanmaku is not totally cleared after releasing track."
         }
     }
-    
+
     @UiThread
     fun setPaused(pause: Boolean) {
         paused = pause
     }
-    
+
     private class UIContext {
         lateinit var baseStyle: TextStyle
         lateinit var textMeasurer: TextMeasurer
         lateinit var density: Density
-        
+
         private val setDeferred: CompletableDeferred<Unit> = CompletableDeferred()
-        
+
         fun set(baseStyle: TextStyle, textMeasurer: TextMeasurer, density: Density) {
             this.baseStyle = baseStyle
             this.textMeasurer = textMeasurer
             this.density = density
-            
+
             setDeferred.complete(Unit)
         }
 
@@ -613,7 +618,7 @@ class DanmakuHostState(
     }
 }
 
-private fun <D : SizeSpecifiedDanmaku, DT, T : DanmakuTrack<D, DT>> 
+private fun <D : SizeSpecifiedDanmaku, DT, T : DanmakuTrack<D, DT>>
         MutableList<T>.setTrackCountImpl(count: Int, newInstance: (index: Int) -> T) {
     when {
         size == count -> return
