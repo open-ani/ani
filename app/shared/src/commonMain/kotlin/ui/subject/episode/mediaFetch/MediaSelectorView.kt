141f866b606e2f30be854d678e9a81016e9930b6
Him188 <Him188@mamoe.net>
2024-09-07 00:22:25 +0100

    Fix MediaSelectorView sticky header background color in EpisodeVideoMediaSelectorSideSheet

--- app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/MediaSelectorView.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/MediaSelectorView.kt
@@ -1,5 +1,6 @@
 package me.him188.ani.app.ui.subject.episode.mediaFetch
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -29,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import me.him188.ani.app.tools.formatDateTime
@@ -52,6 +54,7 @@ fun MediaSelectorView(
     state: MediaSelectorPresentation,
     sourceResults: @Composable LazyItemScope.() -> Unit,
     modifier: Modifier = Modifier,
+    stickyHeaderBackgroundColor: Color = Color.Unspecified,
     itemProgressBar: @Composable RowScope.(Media) -> Unit = {
         FastLinearProgressIndicator(
             state.selected == it,
@@ -82,7 +85,8 @@ fun MediaSelectorView(
                     }
                 }
                 Column(
-                    Modifier.animateItem().padding(bottom = 12.dp).fillMaxWidth(),
+                    Modifier.animateItem().background(stickyHeaderBackgroundColor).padding(bottom = 12.dp)
+                        .fillMaxWidth(),
                     verticalArrangement = Arrangement.spacedBy(8.dp),
                 ) {
                     Text(
