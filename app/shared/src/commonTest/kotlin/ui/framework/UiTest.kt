1d7e98a6cdc495318cbe1f7cfa667f1be8972728
Him188 <Him188@mamoe.net>
2024-09-07 08:55:35 +0100

    Extract Compose state test from ui test

--- app/shared/src/commonTest/kotlin/ui/framework/UiTest.kt
+++ app/shared/src/commonTest/kotlin/ui/framework/UiTest.kt
@@ -1,8 +1,6 @@
 package me.him188.ani.app.ui.framework
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.snapshots.Snapshot
 import androidx.compose.ui.test.ComposeUiTest
 import androidx.compose.ui.test.IdlingResource
 import androidx.compose.ui.test.MainTestClock
@@ -12,58 +10,6 @@ import androidx.compose.ui.test.SemanticsNodeInteractionCollection
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 import androidx.compose.ui.test.runComposeUiTest
 import androidx.compose.ui.unit.Density
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.currentCoroutineContext
-import kotlinx.coroutines.test.TestCoroutineScheduler
-import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import kotlinx.coroutines.yield
-import me.him188.ani.app.ui.foundation.HasBackgroundScope
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
-
-/**
- * [Dispatchers.Main] 设置为 test dispatcher, 让一些 state 操作与测试代码在同一调度队列执行. (例如 [HasBackgroundScope.produceState] 会在 [Dispatchers.Main] 执行)
- *
- * 可以使用 [TestCoroutineScheduler.runCurrent] 来执行所有后台任务.
- * 使用 [takeSnapshot] 来让所有 Compose snapshot state [androidx.compose.runtime.State] 更新, 例如 [derivedStateOf]
- */
-fun runComposeStateTest(
-    context: CoroutineContext = EmptyCoroutineContext,
-    testBody: suspend TestScope.() -> Unit
-) = runTest(context) {
-    setDispatcher()
-    testBody()
-}
-
-fun runComposeStateTest(
-    context: CoroutineContext = EmptyCoroutineContext,
-    timeout: Duration = 60.seconds,
-    testBody: suspend TestScope.() -> Unit
-) = runTest(context, timeout) {
-    setDispatcher()
-    testBody()
-}
-
-@OptIn(ExperimentalStdlibApi::class)
-private suspend inline fun setDispatcher() {
-    Dispatchers.setMain(currentCoroutineContext()[CoroutineDispatcher]!!)
-}
-
-suspend fun TestScope.takeSnapshot() {
-    // magic
-    yield()
-    testScheduler.runCurrent()
-    Snapshot.sendApplyNotifications()
-}
-
-///////////////////////////////////////////////////////////////////////////
-// runAniComposeUiTest
-///////////////////////////////////////////////////////////////////////////
 
 /**
  * 相对于 [runComposeUiTest], 有一些修改:
