open-ani/ani

commit ab5088c1aa8d49c4bf530e83e8380a32db388f69

拆分 :app:shared (#935)
* Separate :app:shared modules

* Fix compilation errors

* Provide DanmakuRegexFilterRepository in tests

* Rename appBase to appPlatform

* Update CONTRIBUTING.md on new architecture

* Increase default Gradle heap limit

* CI: setup graalvm 17

* Revert "CI: Use GraalVM"

This reverts commit eecdb615

app/desktop/build.gradle.kts
@@ -35,6 +35,7 @@ plugins {
 
 dependencies {
     implementation(projects.app.shared)
+    implementation(projects.app.shared.application)
     implementation(compose.components.resources)
     implementation(libs.log4j.core)
     implementation(libs.vlcj)

app/android/build.gradle.kts
@@ -28,6 +28,7 @@ plugins {
 
 dependencies {
     implementation(projects.app.shared)
+    implementation(projects.app.shared.application)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)

app/shared/app-data/build.gradle.kts
@@ -1,0 +1,76 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    kotlin("plugin.compose")
+    id("org.jetbrains.compose")
+
+    `ani-mpp-lib-targets`
+    kotlin("plugin.serialization")
+    id("org.jetbrains.kotlinx.atomicfu")
+
+    id("com.google.devtools.ksp")
+    id("androidx.room")
+    idea
+}
+
+kotlin {
+    sourceSets.commonMain.dependencies {
+        implementation(projects.app.shared.appPlatform)
+        api(projects.app.shared.videoPlayer.videoPlayerApi)
+        api(projects.app.shared.videoPlayer.torrentSource)
+        api(libs.kotlinx.coroutines.core)
+        api(libs.kotlinx.serialization.core)
+        api(libs.kotlinx.collections.immutable)
+        implementation(libs.kotlinx.serialization.json)
+        implementation(projects.utils.io)
+        implementation(projects.utils.coroutines)
+        api(projects.danmaku.danmakuUiConfig)
+        api(projects.utils.xml)
+        api(projects.client)
+        api(projects.utils.ipParser)
+        
+        api(projects.torrent.torrentApi)
+        api(projects.torrent.anitorrent)
+
+        api(libs.datastore.core) // Data Persistence
+        api(libs.datastore.preferences.core) // Preferences
+        api(libs.androidx.room.runtime)
+        api(libs.sqlite.bundled)
+
+        api(projects.datasource.datasourceApi)
+        api(projects.datasource.datasourceCore)
+        api(projects.datasource.bangumi)
+        api(projects.datasource.mikan)
+        api(projects.danmaku.danmakuApi)
+        api(projects.danmaku.dandanplay)
+
+        implementation(libs.koin.core)
+    }
+    sourceSets.commonTest.dependencies {
+        implementation(projects.utils.uiTesting)
+    }
+    sourceSets.androidMain.dependencies {
+        implementation(libs.androidx.compose.ui.tooling.preview)
+        implementation(libs.androidx.compose.ui.tooling)
+    }
+    sourceSets.nativeMain.dependencies {
+        implementation(libs.stately.common) // fixes koin bug
+        implementation(libs.kotlinx.serialization.json.io)
+    }
+}
+
+android {
+    namespace = "me.him188.ani.app.data"
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
+}
+
+dependencies {
+    add("kspDesktop", libs.androidx.room.compiler)
+    add("kspAndroid", libs.androidx.room.compiler)
+    add("kspIosArm64", libs.androidx.room.compiler)
+    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+}

app/shared/app-platform/build.gradle.kts
@@ -1,0 +1,170 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompileTool
+
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    kotlin("plugin.compose")
+    id("org.jetbrains.compose")
+
+    `ani-mpp-lib-targets`
+    kotlin("plugin.serialization")
+    id("org.jetbrains.kotlinx.atomicfu")
+    idea
+}
+
+kotlin {
+    sourceSets.commonMain.dependencies {
+        api(projects.utils.platform)
+        api(libs.kotlinx.coroutines.core)
+        api(projects.danmaku.danmakuApi)
+        api(libs.kotlinx.collections.immutable)
+        api(projects.app.shared.imageViewer)
+
+        api(libs.coil.compose.core)
+        api(libs.coil.svg)
+        api(libs.coil.network.ktor2)
+
+        api(libs.compose.lifecycle.viewmodel.compose)
+        api(libs.compose.lifecycle.runtime.compose)
+        api(libs.compose.navigation.compose)
+        api(libs.compose.navigation.runtime)
+        api(libs.compose.material3.adaptive.core.get().toString()) {
+            exclude("androidx.window.core", "window-core")
+        }
+        api(libs.compose.material3.adaptive.layout.get().toString()) {
+            exclude("androidx.window.core", "window-core")
+        }
+        api(libs.compose.material3.adaptive.navigation0.get().toString()) {
+            exclude("androidx.window.core", "window-core")
+        }
+
+        api(libs.koin.core)
+    }
+    sourceSets.commonTest.dependencies {
+        api(projects.utils.uiTesting)
+    }
+    sourceSets.androidMain.dependencies {
+        api(libs.androidx.compose.ui.tooling.preview)
+        api(libs.androidx.compose.ui.tooling)
+    }
+}
+
+android {
+    namespace = "me.him188.ani.app.platform"
+}
+
+
+val aniAuthServerUrlDebug =
+    getPropertyOrNull("ani.auth.server.url.debug") ?: "https://auth.myani.org"
+val aniAuthServerUrlRelease = getPropertyOrNull("ani.auth.server.url.release") ?: "https://auth.myani.org"
+
+//if (bangumiClientDesktopAppId == null || bangumiClientDesktopSecret == null) {
+//    logger.warn("bangumi.oauth.client.desktop.appId or bangumi.oauth.client.desktop.secret is not set. Bangumi authorization will not work. Get a token from https://bgm.tv/dev/app and set them in local.properties.")
+//}
+
+android {
+    defaultConfig {
+        buildConfigField("String", "VERSION_NAME", "\"${getProperty("version.name")}\"")
+    }
+    buildTypes.getByName("release") {
+        isMinifyEnabled = false
+        isShrinkResources = false
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            *sharedAndroidProguardRules(),
+        )
+        buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani\"")
+        buildConfigField("String", "ANI_AUTH_SERVER_URL", "\"$aniAuthServerUrlRelease\"")
+    }
+    buildTypes.getByName("debug") {
+        buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani.debug2\"")
+        buildConfigField("String", "ANI_AUTH_SERVER_URL", "\"$aniAuthServerUrlDebug\"")
+    }
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+/// BUILD CONFIG
+
+val buildConfigDesktopDir = layout.buildDirectory.file("generated/source/buildConfigDesktop")
+val buildConfigIosDir = layout.buildDirectory.file("generated/source/buildConfigIos")
+
+idea {
+    module {
+        generatedSourceDirs.add(buildConfigDesktopDir.get().asFile)
+        generatedSourceDirs.add(buildConfigIosDir.get().asFile)
+    }
+}
+
+kotlin.sourceSets.getByName("desktopMain") {
+    kotlin.srcDirs(buildConfigDesktopDir)
+}
+
+kotlin.sourceSets.iosMain {
+    kotlin.srcDirs(buildConfigIosDir)
+}
+
+val generateAniBuildConfigDesktop = tasks.register("generateAniBuildConfigDesktop") {
+    val file = buildConfigDesktopDir.get().asFile.resolve("AniBuildConfig.kt").apply {
+        parentFile.mkdirs()
+        createNewFile()
+    }
+
+    inputs.property("project.version", project.version)
+
+    outputs.file(file)
+
+    val text = """
+            package me.him188.ani.app.platform
+            object AniBuildConfigDesktop : AniBuildConfig {
+                override val versionName = "${project.version}"
+                override val isDebug = System.getenv("ANI_DEBUG") == "true" || System.getProperty("ani.debug") == "true"
+                override val aniAuthServerUrl = if (isDebug) "$aniAuthServerUrlDebug" else "$aniAuthServerUrlRelease"
+            }
+            """.trimIndent()
+
+    outputs.upToDateWhen {
+        file.exists() && file.readText().trim() == text.trim()
+    }
+
+    doLast {
+        file.writeText(text)
+    }
+}
+
+val generateAniBuildConfigIos = tasks.register("generateAniBuildConfigIos") {
+    val file = buildConfigIosDir.get().asFile.resolve("AniBuildConfig.kt").apply {
+        parentFile.mkdirs()
+        createNewFile()
+    }
+
+    inputs.property("project.version", project.version)
+
+    outputs.file(file)
+
+    val text = """
+            package me.him188.ani.app.platform
+            object AniBuildConfigIos : AniBuildConfig {
+                override val versionName = "${project.version}"
+                override val isDebug = false
+                override val aniAuthServerUrl = if (isDebug) "$aniAuthServerUrlDebug" else "$aniAuthServerUrlRelease"
+            }
+            """.trimIndent()
+
+    outputs.upToDateWhen {
+        file.exists() && file.readText().trim() == text.trim()
+    }
+
+    doLast {
+        file.writeText(text)
+    }
+}
+
+tasks.named("compileKotlinDesktop") {
+    dependsOn(generateAniBuildConfigDesktop)
+}
+
+tasks.withType(KotlinCompileTool::class) {
+    dependsOn(generateAniBuildConfigIos)
+}

app/shared/application/build.gradle.kts
@@ -1,0 +1,30 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    kotlin("plugin.compose")
+    id("org.jetbrains.compose")
+
+    `ani-mpp-lib-targets`
+    kotlin("plugin.serialization")
+    id("org.jetbrains.kotlinx.atomicfu")
+}
+
+kotlin {
+    sourceSets.commonMain.dependencies {
+        api(projects.app.shared.appPlatform)
+        api(projects.app.shared.uiFoundation)
+        api(projects.app.shared)
+        api(libs.kotlinx.coroutines.core)
+    }
+    sourceSets.commonTest.dependencies {
+        implementation(projects.utils.uiTesting)
+    }
+    sourceSets.androidMain.dependencies {
+        implementation(libs.androidx.compose.ui.tooling.preview)
+        implementation(libs.androidx.compose.ui.tooling)
+    }
+}
+
+android {
+    namespace = "me.him188.ani.app.application"
+}

app/shared/build.gradle.kts
@@ -18,8 +18,6 @@
 
 @file:Suppress("UnstableApiUsage")
 
-import com.google.devtools.ksp.gradle.KspTaskJvm
-import com.google.devtools.ksp.gradle.KspTaskNative
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompileTool
 
 
@@ -34,12 +32,8 @@ plugins {
 
     kotlin("plugin.serialization")
     id("org.jetbrains.kotlinx.atomicfu")
-    id("com.google.devtools.ksp")
-    id("androidx.room")
     idea
 }
-
-extra.set("ani.jvm.target", 17)
 
 compose.resources {
     packageOfResClass = "me.him188.ani.app"
@@ -76,6 +70,13 @@ kotlin {
         api(libs.kotlinx.io.core)
         api(libs.kotlinx.collections.immutable)
         api(projects.utils.ktorClient)
+
+        api(projects.app.shared.appPlatform)
+        api(projects.app.shared.appData)
+        api(projects.app.shared.placeholder)
+        api(projects.app.shared.uiFoundation)
+        api(projects.app.shared.videoPlayer)
+        api(projects.app.shared.videoPlayer.torrentSource)
 
         // Compose
         api(compose.foundation)
@@ -128,18 +129,12 @@ kotlin {
         api(libs.ktor.serialization.kotlinx.json)
 
         // Others
-        api(libs.koin.core) // dependency injection
+        api(libs.koin.core) 
         api(libs.directories) // Data directories on all OSes
         api(libs.coil.core)
         api(libs.coil.svg)
         api(libs.coil.compose.core)
         api(libs.coil.network.ktor2)
-        api(libs.datastore.core) // Data Persistence
-        api(libs.datastore.preferences.core) // Preferences
-        implementation(libs.androidx.room.runtime.get().toString()) {
-            exclude("org.jetbrains.kotlinx", "atomicfu")
-        } // multi-platform database
-        api(libs.sqlite.bundled) // database driver implementation
         implementation(libs.constraintlayout.compose)
     }
 
@@ -211,11 +206,7 @@ kotlin {
         api(libs.kotlinx.coroutines.swing)
         implementation(libs.vlcj)
         implementation(libs.jna) // required and don't change version, otherwise vlcj might crash the VM 
-//        implementation(libs.vlcj.javafx)
-//        implementation(libs.javafx.controls)
-//        implementation(libs.javafx.graphics)
 
-        // https://repo1.maven.org/maven2/org/openjfx/javafx-graphics/17.0.11/
         runtimeOnly(libs.kotlinx.coroutines.debug)
 
         implementation(libs.log4j.core)
@@ -228,75 +219,6 @@ kotlin {
 
         implementation(libs.filekit.core)
         implementation(libs.filekit.compose)
-//        implementation(libs.htmlunit)
-//        implementation("org.openjfx:javafx-base:17.0.11:$classifier") {
-//            exclude("org.openjfx")
-//        }
-//        implementation("org.openjfx:javafx-controls:17.0.11:$classifier") {
-//            exclude("org.openjfx")
-//        }
-//        implementation("org.openjfx:javafx-graphics:17.0.11:$classifier") {
-//            exclude("org.openjfx")
-//        }
-//        implementation("org.openjfx:javafx-media:17.0.11:$classifier") {
-//            exclude("org.openjfx")
-//        }
-//        implementation("org.openjfx:javafx-web:17.0.11:$classifier") {
-//            exclude("org.openjfx")
-//        }
-    }
-
-    sourceSets {
-        // TODO: 临时解决方案, KT-65362 Cannot resolve declarations from a dependency when there are multiple JVM-only project dependencies in a JVM-Android MPP
-        //  https://youtrack.jetbrains.com/issue/KT-65362
-        // Danmaku
-
-        fun submodule(dir: String, flatten: Boolean = !rootProject.projectDir.resolve("$dir/src/commonMain").exists()) {
-            if (flatten) {
-                // flatten
-                commonMain {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/src/"))
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/commonMain/"))
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/common/"))
-                    resources.srcDirs(rootProject.projectDir.resolve("$dir/resources/"))
-                }
-                commonTest {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/test/"))
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/commonTest/"))
-                }
-                androidMain {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/androidMain/"))
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/android/"))
-                }
-                getByName("desktopMain") {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/desktopMain/"))
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/desktop/"))
-                }
-                commonTest {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/commonTest/"))
-                }
-                getByName("androidUnitTest") {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/androidUnitTest/"))
-                }
-                getByName("desktopTest") {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/desktopTest/"))
-                }
-                getByName("iosMain") {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/ios/"))
-                }
-                getByName("skikoMain") {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/skikoMain"))
-                }
-            } else {
-                sourceSets.all {
-                    kotlin.srcDirs(rootProject.projectDir.resolve("$dir/src/${this.name}/kotlin"))
-                    resources.srcDirs(rootProject.projectDir.resolve("$dir/src/${this.name}/resources"))
-                }
-            }
-        }
-
-        submodule("app/shared/placeholder")
-        submodule("app/shared/video-player")
     }
 }
 
@@ -310,7 +232,7 @@ idea {
     }
 }
 
-// AS 问题 since 1.7.0-beta03
+// AS 问题 since Compose 1.7.0-beta03
 afterEvaluate {
     tasks.matching { it.name.contains("generateReleaseLintVitalModel") }.all {
         dependsOn("releaseAssetsCopyForAGP")
@@ -320,16 +242,8 @@ afterEvaluate {
     }
 }
 
-room {
-    schemaDirectory("$projectDir/schemas")
-}
-
 
 // BUILD CONFIG
-
-val aniAuthServerUrlDebug =
-    getPropertyOrNull("ani.auth.server.url.debug") ?: "https://auth.myani.org"
-val aniAuthServerUrlRelease = getPropertyOrNull("ani.auth.server.url.release") ?: "https://auth.myani.org"
 
 //if (bangumiClientDesktopAppId == null || bangumiClientDesktopSecret == null) {
 //    logger.warn("bangumi.oauth.client.desktop.appId or bangumi.oauth.client.desktop.secret is not set. Bangumi authorization will not work. Get a token from https://bgm.tv/dev/app and set them in local.properties.")
@@ -340,21 +254,18 @@ android {
     compileSdk = getIntProperty("android.compile.sdk")
     defaultConfig {
         minSdk = getIntProperty("android.min.sdk")
-        buildConfigField("String", "VERSION_NAME", "\"${getProperty("version.name")}\"")
     }
     buildTypes.getByName("release") {
-        isMinifyEnabled = false // shared 不能 minify, 否则构建 app 会失败
+        isMinifyEnabled = false
         isShrinkResources = false
         proguardFiles(
             getDefaultProguardFile("proguard-android-optimize.txt"),
             *sharedAndroidProguardRules(),
         )
         buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani\"")
-        buildConfigField("String", "ANI_AUTH_SERVER_URL", "\"$aniAuthServerUrlRelease\"")
     }
     buildTypes.getByName("debug") {
         buildConfigField("String", "APP_APPLICATION_ID", "\"me.him188.ani.debug2\"")
-        buildConfigField("String", "ANI_AUTH_SERVER_URL", "\"$aniAuthServerUrlDebug\"")
     }
     buildFeatures {
         compose = true
@@ -363,111 +274,9 @@ android {
 }
 
 dependencies {
-    add("kspDesktop", libs.androidx.room.compiler)
-    add("kspAndroid", libs.androidx.room.compiler)
-    add("kspIosArm64", libs.androidx.room.compiler)
-    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
 
-
-val buildConfigDesktopDir = layout.buildDirectory.file("generated/source/buildConfigDesktop")
-val buildConfigIosDir = layout.buildDirectory.file("generated/source/buildConfigIos")
-
-idea {
-    module {
-        generatedSourceDirs.add(buildConfigDesktopDir.get().asFile)
-        generatedSourceDirs.add(buildConfigIosDir.get().asFile)
-    }
-}
-
-kotlin.sourceSets.getByName("desktopMain") {
-    kotlin.srcDirs(buildConfigDesktopDir)
-}
-
-kotlin.sourceSets.iosMain {
-    kotlin.srcDirs(buildConfigIosDir)
-}
-
-//tasks.register("generateBuildConfigForDesktop") {
-//    doLast {
-//        
-//    }
-//}
-val generateAniBuildConfigDesktop = tasks.register("generateAniBuildConfigDesktop") {
-    val file = buildConfigDesktopDir.get().asFile.resolve("AniBuildConfig.kt").apply {
-        parentFile.mkdirs()
-        createNewFile()
-    }
-
-    inputs.property("project.version", project.version)
-
-    outputs.file(file)
-
-    val text = """
-            package me.him188.ani.app.platform
-            object AniBuildConfigDesktop : AniBuildConfig {
-                override val versionName = "${project.version}"
-                override val isDebug = System.getenv("ANI_DEBUG") == "true" || System.getProperty("ani.debug") == "true"
-                override val aniAuthServerUrl = if (isDebug) "$aniAuthServerUrlDebug" else "$aniAuthServerUrlRelease"
-            }
-            """.trimIndent()
-
-    outputs.upToDateWhen {
-        file.exists() && file.readText().trim() == text.trim()
-    }
-
-    doLast {
-        file.writeText(text)
-    }
-}
-
-val generateAniBuildConfigIos = tasks.register("generateAniBuildConfigIos") {
-    val file = buildConfigIosDir.get().asFile.resolve("AniBuildConfig.kt").apply {
-        parentFile.mkdirs()
-        createNewFile()
-    }
-
-    inputs.property("project.version", project.version)
-
-    outputs.file(file)
-
-    val text = """
-            package me.him188.ani.app.platform
-            object AniBuildConfigIos : AniBuildConfig {
-                override val versionName = "${project.version}"
-                override val isDebug = false
-                override val aniAuthServerUrl = if (isDebug) "$aniAuthServerUrlDebug" else "$aniAuthServerUrlRelease"
-            }
-            """.trimIndent()
-
-    outputs.upToDateWhen {
-        file.exists() && file.readText().trim() == text.trim()
-    }
-
-    doLast {
-        file.writeText(text)
-    }
-}
-
-tasks.named("compileKotlinDesktop") {
-    dependsOn(generateAniBuildConfigDesktop)
-}
-
-tasks.withType(KotlinCompileTool::class) {
-    dependsOn(generateAniBuildConfigIos)
-}
-
-// :app:shared:kspKotlinDesktop
-tasks.withType(KspTaskJvm::class.java) {
-    dependsOn(generateAniBuildConfigDesktop)
-}
-tasks.matching { it.name.startsWith("kspKotlin") }.all {
-    dependsOn(generateAniBuildConfigDesktop)
-}
-tasks.withType(KspTaskNative::class.java) {
-    dependsOn(generateAniBuildConfigIos)
-}
 
 // 太耗内存了, 只能一次跑一个
 // compose bug, 不能用这个 https://youtrack.jetbrains.com/issue/CMP-5835
@@ -482,6 +291,7 @@ tasks.withType(KspTaskNative::class.java) {
 //    }
 
 if (enableIosFramework) {
+// 太耗内存了, 只能一次跑一个
     tasks.named("linkDebugFrameworkIosArm64") {
         mustRunAfter("linkReleaseFrameworkIosArm64")
         mustRunAfter("linkDebugFrameworkIosSimulatorArm64")

app/shared/placeholder/build.gradle.kts
@@ -1,0 +1,31 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    kotlin("plugin.compose")
+    id("org.jetbrains.compose")
+
+    `ani-mpp-lib-targets`
+    kotlin("plugin.serialization")
+    id("org.jetbrains.kotlinx.atomicfu")
+}
+
+kotlin {
+    sourceSets.commonMain.dependencies {
+        api(compose.material3)
+        implementation(libs.androidx.annotation)
+    }
+    sourceSets.commonTest.dependencies {
+        implementation(projects.utils.uiTesting)
+    }
+    sourceSets.androidMain.dependencies {
+        implementation(libs.androidx.compose.ui.tooling.preview)
+        implementation(libs.androidx.compose.ui.tooling)
+        implementation(libs.compose.material3.adaptive.core.get().toString()) {
+            exclude("androidx.window.core", "window-core")
+        }
+    }
+}
+
+android {
+    namespace = "me.him188.ani.app.placeholder"
+}

app/shared/ui-foundation/build.gradle.kts
@@ -1,0 +1,66 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    kotlin("plugin.compose")
+    id("org.jetbrains.compose")
+
+    `ani-mpp-lib-targets`
+    kotlin("plugin.serialization")
+    id("org.jetbrains.kotlinx.atomicfu")
+}
+
+kotlin {
+    sourceSets.commonMain.dependencies {
+        implementation(projects.app.shared.appData)
+        implementation(projects.app.shared.appPlatform)
+        implementation(projects.utils.platform)
+        implementation(libs.kotlinx.coroutines.core)
+        implementation(projects.danmaku.danmakuApi)
+        implementation(libs.kotlinx.collections.immutable)
+        implementation(libs.kotlinx.serialization.protobuf)
+        implementation(projects.app.shared.placeholder)
+        
+        api(libs.coil.compose.core)
+        api(libs.coil.svg)
+        api(libs.coil.network.ktor2)
+
+        implementation(compose.components.resources)
+        api(libs.compose.lifecycle.viewmodel.compose)
+        api(libs.compose.lifecycle.runtime.compose)
+        api(libs.compose.navigation.compose)
+        api(libs.compose.navigation.runtime)
+        api(libs.compose.material3.adaptive.core.get().toString()) {
+            exclude("androidx.window.core", "window-core")
+        }
+        api(libs.compose.material3.adaptive.layout.get().toString()) {
+            exclude("androidx.window.core", "window-core")
+        }
+        api(libs.compose.material3.adaptive.navigation0.get().toString()) {
+            exclude("androidx.window.core", "window-core")
+        }
+
+        implementation(projects.utils.bbcode)
+        implementation(libs.constraintlayout.compose)
+        
+        api(libs.koin.core)
+    }
+    sourceSets.commonTest.dependencies {
+        implementation(projects.utils.uiTesting)
+    }
+    sourceSets.androidMain.dependencies {
+        implementation(libs.androidx.compose.ui.tooling.preview)
+        implementation(libs.androidx.compose.ui.tooling)
+        implementation(libs.compose.material3.adaptive.core.get().toString()) {
+            exclude("androidx.window.core", "window-core")
+        }
+        // Preview only
+    }
+    sourceSets.desktopMain.dependencies {
+        implementation(libs.jna)
+        implementation(libs.jna.platform)
+    }
+}
+
+android {
+    namespace = "me.him188.ani.app.foundation"
+}

commit 059ea02f884cc583a9b033c3e55c70b6cf11fbb4

Workaround missing compose resource in "Run Desktop".
caused by ab5088c1aa8d49c4bf530e83e8380a32db388f69

app/desktop/build.gradle.kts
@@ -35,6 +35,7 @@ plugins {
 
 dependencies {
     implementation(projects.app.shared)
+    implementation(projects.app.shared.uiFoundation)
     implementation(projects.app.shared.application)
     implementation(compose.components.resources)
     implementation(libs.log4j.core)
@@ -44,13 +45,15 @@ dependencies {
 // workaround for compose limitation
 tasks.named("processResources") {
     dependsOn(":app:shared:desktopProcessResources")
+    dependsOn(":app:shared:ui-foundation:desktopProcessResources")
 }
 
 sourceSets {
     main {
         resources.srcDirs(
-            project(":app:shared").layout.buildDirectory
-//                .file("generated/compose/resourceGenerator/preparedResources/commonMain")
+            projects.app.shared.dependencyProject.layout.buildDirectory
+                .file("processedResources/desktop/main"),
+            projects.app.shared.uiFoundation.dependencyProject.layout.buildDirectory
                 .file("processedResources/desktop/main"),
         )
     }