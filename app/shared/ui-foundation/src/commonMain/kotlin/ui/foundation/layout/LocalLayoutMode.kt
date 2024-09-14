c91a293ecdb91220fe11542b3e9e1c6c1163a91b
Him188 <Him188@mamoe.net>
2024-09-14 12:31:58 +0100

    Deprecate LocalLayoutMode in favor of currentWindowAdaptiveInfo

--- app/shared/ui-foundation/src/commonMain/kotlin/ui/foundation/layout/LocalLayoutMode.kt
+++ app/shared/ui-foundation/src/commonMain/kotlin/ui/foundation/layout/LocalLayoutMode.kt
@@ -1,4 +1,13 @@
-@file:Suppress("NOTHING_TO_INLINE")
+/*
+ * Copyright 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
+@file:Suppress("NOTHING_TO_INLINE", "KotlinRedundantDiagnosticSuppress")
 
 package me.him188.ani.app.ui.foundation.layout
 
@@ -10,6 +19,7 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.unit.DpSize
 
 @Stable
+@Deprecated("Use currentWindowAdaptiveInfo() instead")
 val LocalLayoutMode: ProvidableCompositionLocal<LayoutMode> = staticCompositionLocalOf {
     error("No LayoutMode provided")
 }
