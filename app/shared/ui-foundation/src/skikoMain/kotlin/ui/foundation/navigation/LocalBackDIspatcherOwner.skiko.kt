1b39b1aacdc18a372993362a550b6c0a75df21f3
Him188 <Him188@mamoe.net>
2024-09-14 12:24:44 +0100

    Make LocalOnBackPressedDispatcherOwner static on Skiko

--- app/shared/ui-foundation/src/skikoMain/kotlin/ui/foundation/navigation/LocalBackDIspatcherOwner.skiko.kt
+++ app/shared/ui-foundation/src/skikoMain/kotlin/ui/foundation/navigation/LocalBackDIspatcherOwner.skiko.kt
@@ -1,14 +1,23 @@
+/*
+ * Copyright 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.ui.foundation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ProvidedValue
-import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.util.fastLastOrNull
 
 actual object LocalOnBackPressedDispatcherOwner {
     private val LocalOnBackPressedDispatcherOwner =
-        compositionLocalOf<OnBackPressedDispatcherOwner?> { null }
+        staticCompositionLocalOf<OnBackPressedDispatcherOwner?> { null }
 
     /**
      * Returns current composition local value for the owner or `null` if one has not
