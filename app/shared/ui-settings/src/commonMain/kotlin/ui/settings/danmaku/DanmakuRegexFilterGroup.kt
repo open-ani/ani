95bb4bcdc66b46f6f9f370e8b7362d3a54e8e5d3
Him188 <Him188@mamoe.net>
2024-09-18 10:37:01 +0100

    AddRegexFilterDialog: use AlertDialog with TextButton

--- app/shared/ui-settings/src/commonMain/kotlin/ui/settings/danmaku/DanmakuRegexFilterGroup.kt
+++ app/shared/ui-settings/src/commonMain/kotlin/ui/settings/danmaku/DanmakuRegexFilterGroup.kt
@@ -1,8 +1,16 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.ui.settings.danmaku
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,14 +20,12 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Close
-import androidx.compose.material3.Button
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -30,7 +36,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
@@ -44,7 +49,6 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import me.him188.ani.app.data.models.danmaku.DanmakuRegexFilter
 import me.him188.ani.app.ui.foundation.ifThen
 import me.him188.ani.app.ui.settings.framework.components.SettingsScope
@@ -176,7 +180,7 @@ fun AddRegexFilterDialog(
     val validRegex by remember { derivedStateOf { isValidRegex(regexTextFieldValue) } }
     var isError by remember { mutableStateOf(false) }
 
-    fun handleAdd(): Unit {
+    fun handleAdd() {
         if (!isBlank && validRegex) {
             isError = false
             onAdd(
@@ -195,91 +199,68 @@ fun AddRegexFilterDialog(
         focusManager.clearFocus()
     }
 
-    Dialog(
+    AlertDialog(
         onDismissRequest = onDismissRequest,
-    ) {
-        Surface(
-            shape = MaterialTheme.shapes.large,
-        ) {
-            Column(Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
-                Row(
-                    Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Start,
-                ) {
-                    ProvideTextStyle(MaterialTheme.typography.titleMedium) {
-                        title()
-                    }
-                }
-
-                Row(
-                    Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Start,
-                ) {
-                    OutlinedTextField(
-                        value = regexTextFieldValue,
-                        onValueChange = {
-                            regexTextFieldValue = it
-                            isError = false
-                        },
-                        label = { Text("正则表达式") },
-                        modifier = Modifier.fillMaxWidth()
-                            .focusRequester(focusRequester)
-                            .onKeyEvent { event: KeyEvent ->
-                                if (event.key == Key.Enter) {
-                                    handleAdd()
-                                    true // Consume the event
-                                } else {
-                                    false // Pass the event to other handlers
-                                }
-                            },
-                        keyboardOptions = KeyboardOptions.Default.copy(
-                            imeAction = ImeAction.Done,
-                        ),
-                        keyboardActions = KeyboardActions(
-                            onDone = { handleAdd() },
-                        ),
-                        supportingText = {
-                            if (isError) {
-                                Text(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    text = "正则表达式语法不正确",
-                                )
-                            } else {
-                                Text(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    text = "填写用于屏蔽的正则表达式，例如：‘.*签.*’ 会屏蔽所有含有文字‘签’的弹幕。",
-                                )
-                            }
-                        },
-                        isError = isError,
-                        singleLine = true,
-                    )
-                }
-
-                Row(
-                    Modifier.fillMaxWidth().align(Alignment.End),
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    TextButton(onClick = onDismissRequest) {
-                        Text("取消")
-                    }
-
-                    Button(
-                        onClick = {
-                            handleAdd()
-                        }, // Pass the text field value to onConfirm
-                        enabled = !isBlank,
-                    ) {
-                        Text("确认")
-                    }
-                }
-
-                LaunchedEffect(Unit) {
-                    focusRequester.requestFocus()
-                }
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    handleAdd()
+                }, // Pass the text field value to onConfirm
+                enabled = !isBlank,
+            ) {
+                Text("确认")
             }
-        }
-    }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text("取消")
+            }
+        },
+        title = title,
+        text = {
+            OutlinedTextField(
+                value = regexTextFieldValue,
+                onValueChange = {
+                    regexTextFieldValue = it
+                    isError = false
+                },
+                label = { Text("正则表达式") },
+                modifier = Modifier.fillMaxWidth()
+                    .focusRequester(focusRequester)
+                    .onKeyEvent { event: KeyEvent ->
+                        if (event.key == Key.Enter) {
+                            handleAdd()
+                            true // Consume the event
+                        } else {
+                            false // Pass the event to other handlers
+                        }
+                    },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = { handleAdd() },
+                ),
+                supportingText = {
+                    if (isError) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "正则表达式语法不正确",
+                        )
+                    } else {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "填写用于屏蔽的正则表达式，例如：‘.*签.*’ 会屏蔽所有含有文字‘签’的弹幕。",
+                        )
+                    }
+                },
+                isError = isError,
+                singleLine = true,
+            )
+            LaunchedEffect(Unit) {
+                focusRequester.requestFocus()
+            }
+        },
+    )
 }
 
