9f03d4b5134d862ed243f3336d5018bc81fe7439
Him188 <Him188@mamoe.net>
2024-09-18 12:55:06 +0100

    Add `@Sample` to be annotated on samples

--- app/shared/src/desktopTest/kotlin/tools/caching/LazyDataCacheSamples.kt
+++ app/shared/src/desktopTest/kotlin/tools/caching/LazyDataCacheSamples.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 @file:Suppress("unused")
 
 package me.him188.ani.app.tools.caching
@@ -5,6 +14,7 @@ package me.him188.ani.app.tools.caching
 import kotlinx.coroutines.flow.flowOf
 import me.him188.ani.app.data.models.ApiResponse
 import me.him188.ani.datasources.api.paging.SinglePagePagedSource
+import me.him188.ani.test.Sample
 
 class LazyDataCacheSamples {
     private data class Subject(
@@ -24,6 +34,7 @@ class LazyDataCacheSamples {
         },
     )
 
+    @Sample
     suspend fun mutate() {
         cache.mutate {
             setEach(where = { it.id == 1 }) {
