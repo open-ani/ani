007b4202ca3bfdc37f529b470b92ea92976ad953
Him188 <Him188@mamoe.net>
2024-09-20 16:07:17 +0100

    Log factory id when MediaSourceFactory could not be found

--- app/shared/app-data/src/commonMain/kotlin/data/source/media/fetch/MediaSourceManager.kt
+++ app/shared/app-data/src/commonMain/kotlin/data/source/media/fetch/MediaSourceManager.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.data.source.media.fetch
 
 import kotlinx.coroutines.CoroutineExceptionHandler
@@ -209,7 +218,7 @@ class MediaSourceManagerImpl(
     private fun createInstance(save: MediaSourceSave, config: MediaSourceProxySettings): MediaSourceInstance? {
         val factory = factories.find { it.factoryId == save.factoryId }
         return if (factory == null) {
-            logger.error { "MediaSourceFactory not found for ${save.mediaSourceId}" }
+            logger.error { "MediaSourceFactory '${save.factoryId}' not found for ${save.mediaSourceId}" }
             null
         } else {
             MediaSourceInstance(
