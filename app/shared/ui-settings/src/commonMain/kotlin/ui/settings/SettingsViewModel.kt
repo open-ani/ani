98da56583a7a9199f1a00e50a8795565dfe55783
Him188 <Him188@mamoe.net>
2024-09-23 21:54:03 +0100

    修复初次添加 RSS/Selector 数据源时编辑配置无效

--- app/shared/ui-settings/src/commonMain/kotlin/ui/settings/SettingsViewModel.kt
+++ app/shared/ui-settings/src/commonMain/kotlin/ui/settings/SettingsViewModel.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.ui.settings
 
 import androidx.compose.runtime.derivedStateOf
@@ -41,7 +50,6 @@ import me.him188.ani.datasources.api.source.asAutoCloseable
 import me.him188.ani.datasources.api.subject.SubjectProvider
 import me.him188.ani.datasources.bangumi.BangumiSubjectProvider
 import me.him188.ani.utils.ktor.createDefaultHttpClient
-import me.him188.ani.utils.platform.Uuid
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -177,8 +185,7 @@ class SettingsViewModel : AbstractSettingsViewModel(), KoinComponent {
                 checkNotNull(it) { "Could not find MediaSourceConfig for id $id" }
             }
         },
-        onAdd = { factoryId, config ->
-            val instanceId = Uuid.randomString()
+        onAdd = { factoryId, instanceId, config ->
             mediaSourceManager.addInstance(instanceId, instanceId, factoryId, config)
         },
         onEdit = { instanceId, config -> mediaSourceManager.updateConfig(instanceId, config) },
