10ea2e98f29ddbd8630dcfb65a2f5b1e06054b38
Him188 <Him188@mamoe.net>
2024-09-24 19:43:40 +0100

    Fix #1006 条目缓存 media selector sticky header 背景透明

--- app/shared/src/commonMain/kotlin/ui/subject/cache/CacheListGroup.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/cache/CacheListGroup.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 @file:Suppress("PropertyName")
 
 package me.him188.ani.app.ui.subject.cache
@@ -157,6 +166,7 @@ fun SettingsScope.EpisodeCacheListGroup(
                 MediaSelectorView(
                     selectorPresentation,
                     sourceResults = { MediaSourceResultsView(sourceResults, selectorPresentation) },
+                    stickyHeaderBackgroundColor = BottomSheetDefaults.ContainerColor,
                     modifier = Modifier.padding(vertical = 12.dp, horizontal = 16.dp)
                         .navigationBarsPadding()
                         .fillMaxHeight() // 防止添加筛选后数量变少导致 bottom sheet 高度变化
