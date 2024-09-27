696abb76aa5ad29b76f8f74658eedc124a08573b
Him188 <Him188@mamoe.net>
2024-09-27 22:55:49 +0100

    在匹配条目名称时, 忽略特殊字符

--- app/shared/app-data/src/commonMain/kotlin/data/source/media/source/MediaListFilterContext.kt
+++ app/shared/app-data/src/commonMain/kotlin/data/source/media/source/MediaListFilterContext.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.data.source.media.source
 
 import me.him188.ani.app.data.source.media.source.MediaListFilter.Candidate
@@ -12,8 +21,10 @@ open class MediaListFilterContext(
     val subjectNames: Set<String>, // faster
     val episodeSort: EpisodeSort,
 ) {
-    val subjectNamesNoSpace: Set<String> by lazy {
-        subjectNames.mapTo(HashSet(subjectNames.size)) { it.replace(" ", "") }
+    val subjectNamesWithoutSpecial: Set<String> by lazy {
+        subjectNames.mapTo(HashSet(subjectNames.size)) {
+            it.replace(MediaListFilters.specialCharRegex, "")
+        }
     }
 
     /**
--- app/shared/app-data/src/commonMain/kotlin/data/source/media/source/MediaListFilters.kt
+++ app/shared/app-data/src/commonMain/kotlin/data/source/media/source/MediaListFilters.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.data.source.media.source
 
 import me.him188.ani.datasources.api.topic.contains
@@ -8,9 +17,11 @@ import me.him188.ani.datasources.api.topic.contains
  * @see MediaListFilter
  */
 object MediaListFilters {
+    val specialCharRegex = Regex("""[ 	~!@#$%^&*()_+{}\[\]\\|;':",.<>/?【】]""")
+    
     val ContainsSubjectName = BasicMediaListFilter { media ->
-        subjectNamesNoSpace.any { subjectName ->
-            media.originalTitle.replace(" ", "")
+        subjectNamesWithoutSpecial.any { subjectName ->
+            media.originalTitle.replace(specialCharRegex, "")
                 .contains(subjectName, ignoreCase = true)
         }
     }
