f65cb06da52aedef5950d4aea06607b2ac674407
Him188 <Him188@mamoe.net>
2024-09-28 22:40:26 +0100

    Add MediaListFilter.or

--- app/shared/app-data/src/commonMain/kotlin/data/source/media/source/MediaListFilter.kt
+++ app/shared/app-data/src/commonMain/kotlin/data/source/media/source/MediaListFilter.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.data.source.media.source
 
 import me.him188.ani.datasources.api.Media
@@ -36,6 +45,13 @@ fun interface MediaListFilter<in Ctx : MediaListFilterContext> {
      * 返回 `false` 表示不符合, 将会导致该 [Candidate] 被完全从结果中剔除. Media Selector 中的计数也不会包含它.
      */
     fun Ctx.applyOn(media: Candidate): Boolean
+}
+
+infix fun <C : MediaListFilterContext> MediaListFilter<C>.or(cond: MediaListFilter<C>): MediaListFilter<C> {
+    val self = this
+    return MediaListFilter<C> { candidate ->
+        with(self) { applyOn(candidate) } || with(cond) { applyOn(candidate) }
+    }
 }
 
 typealias BasicMediaListFilter = MediaListFilter<MediaListFilterContext>
