81cb2648dd3f08508c084497edf45b4b26328f5c
Him188 <Him188@mamoe.net>
2024-10-03 13:09:40 +0100

    Update foojay resolver version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 fun includeProject(projectPath: String, dir: String? = null) {
