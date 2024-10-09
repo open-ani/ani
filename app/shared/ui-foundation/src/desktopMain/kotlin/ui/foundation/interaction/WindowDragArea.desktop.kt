5484b3b05f5ad4db726ba54a74d2fb189982862b
立直恐惧症 <65274728+NieR4ever@users.noreply.github.com>
2024-10-09 17:50:24 +0800

    在 windows 上禁用 WindowDragArea (#1050)
    * disable WindowDragArea on Windows
    
    * optimize something

--- app/shared/ui-foundation/src/desktopMain/kotlin/ui/foundation/interaction/WindowDragArea.desktop.kt
+++ app/shared/ui-foundation/src/desktopMain/kotlin/ui/foundation/interaction/WindowDragArea.desktop.kt
@@ -12,7 +12,9 @@ package me.him188.ani.app.ui.foundation.interaction
 import androidx.compose.foundation.window.WindowDraggableArea
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import me.him188.ani.app.ui.foundation.LocalPlatform
 import me.him188.ani.app.ui.foundation.layout.LocalPlatformWindow
+import me.him188.ani.utils.platform.Platform
 
 @Composable
 actual inline fun WindowDragArea(
@@ -20,9 +22,13 @@ actual inline fun WindowDragArea(
     crossinline content: @Composable () -> Unit
 ) {
     val windowScope = LocalPlatformWindow.current.windowScope
-    windowScope?.run {
-        WindowDraggableArea(modifier) {
-            content()
-        }
-    } ?: content()
+    if (LocalPlatform.current is Platform.Windows) {
+        content()
+    } else {
+        windowScope?.run {
+            WindowDraggableArea(modifier) {
+                content()
+            }
+        } ?: content()
+    }
 }
