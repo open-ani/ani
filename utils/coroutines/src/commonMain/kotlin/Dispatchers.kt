2795897404af0be268c495ba83706fde6e64b567
Him188 <Him188@mamoe.net>
2024-10-20 11:53:30 +0100

    Add Dispatchers.IO_

---
+++ utils/coroutines/src/commonMain/kotlin/Dispatchers.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
+package me.him188.ani.utils.coroutines
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO as _IO
+
+/**
+ * 跟 `Dispatchers.IO` 一样, 但是在禁用 ios 编译目标后也可以使用 (import 不会被 IDE 自动清除)
+ */
+inline val Dispatchers.IO_: CoroutineDispatcher
+    get() = _IO
