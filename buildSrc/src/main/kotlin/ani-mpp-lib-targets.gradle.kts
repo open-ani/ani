34d59ba6aa4c7d8960bb2af421d2aaecafa99ce0
Him188 <Him188@mamoe.net>
2024-10-24 10:05:46 +0100

    CI: do not enforce JVM toolchain 17

--- buildSrc/src/main/kotlin/ani-mpp-lib-targets.gradle.kts
+++ buildSrc/src/main/kotlin/ani-mpp-lib-targets.gradle.kts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 import com.android.build.api.dsl.LibraryExtension
 import org.jetbrains.compose.ComposeExtension
 import org.jetbrains.compose.ComposePlugin
@@ -14,8 +23,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
  * 
  * 如果开了 android, 就会配置 desktop + android, 否则只配置 jvm.
  */
-
-extra.set("ani.jvm.target", 17)
 
 val android = extensions.findByType(LibraryExtension::class)
 val composeExtension = extensions.findByType(ComposeExtension::class)
