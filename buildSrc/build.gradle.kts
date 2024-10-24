6b7b462b3feee6c7a4363a92b3e181e476960459
Him188 <Him188@mamoe.net>
2024-10-24 10:37:38 +0000

    Use JBR 17 by default in local builds and 21 in CI (#1102)
    * Use JBR 17 by default in local builds and 21 in CI
    
    * fix build

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,17 @@
  * https://github.com/open-ani/ani/blob/main/LICENSE
  */
 
+import java.util.Properties
+
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 plugins {
     `kotlin-dsl`
 }
@@ -51,4 +62,30 @@ dependencies {
     api(libs.kotlin.compose.compiler.gradle.plugin)
     implementation(kotlin("script-runtime"))
     implementation(libs.snakeyaml)
+}
+
+
+
+
+fun Project.getPropertyOrNull(name: String) =
+    getLocalProperty(name)
+        ?: System.getProperty(name)
+        ?: System.getenv(name)
+        ?: findProperty(name)?.toString()
+        ?: properties[name]?.toString()
+        ?: extensions.extraProperties.runCatching { get(name).toString() }.getOrNull()
+
+val Project.localPropertiesFile: File get() = project.rootProject.file("local.properties")
+
+fun Project.getLocalProperty(key: String): String? {
+    return if (localPropertiesFile.exists()) {
+        val properties = Properties()
+        localPropertiesFile.inputStream().buffered().use { input ->
+            properties.load(input)
+        }
+        properties.getProperty(key)
+    } else {
+        localPropertiesFile.createNewFile()
+        null
+    }
 }
